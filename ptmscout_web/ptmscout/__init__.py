8374b151d693b2580a22c634b5e42f585653655e
Matthew Matlock <matt.matlock@gmail.com>
2012-08-22 16:17:58 -0500

    Init now creates DB engine for mysqlDB.

--- ptmscout_web/ptmscout/__init__.py
+++ ptmscout_web/ptmscout/__init__.py
@@ -1,9 +1,14 @@
 from pyramid.config import Configurator
 
+from sqlalchemy import engine_from_config
+from database.models import DBSession
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('login', '/login')
     config.add_route('process_login', '/process_login')
