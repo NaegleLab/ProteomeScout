d0ff583642d38196ad18f955d556c07a0dafbfe4
Matthew Matlock <matt.matlock@gmail.com>
2012-08-22 10:54:11 -0500

    Added resource bindings for login views.

--- ptmscout_web/ptmscout/__init__.py
+++ ptmscout_web/ptmscout/__init__.py
@@ -5,6 +5,8 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('login', '/login')
+    config.add_route('process_login', '/process_login')
     config.add_route('register', '/register')
     config.add_route('process_registration', '/process_registration')
     config.scan()
