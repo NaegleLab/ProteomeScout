8cf6690785cda2b52526060b0c37131c3ccc5f59
Matthew Matlock <matt.matlock@gmail.com>
2012-08-30 14:44:02 -0500

    Added stubbed upload page to test authorization functionality

---
+++ ptmscout_web/ptmscout/upload.py
@@ -0,0 +1,9 @@
+from pyramid.view import view_config
+from ptmscout.layout import site_layout
+
+@view_config(route_name='upload', renderer='templates/information.pt', permission='upload')
+def user_upload(request):    
+    return {'layout': site_layout(),
+            'pageTitle': "Upload",
+            'header': "Data Upload",
+            'message': "Data upload feature is not yet available."}
