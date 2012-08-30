b0f85878bf8792e5c7df6c09fe9d2a66aafe2f96
Matthew Matlock <matt.matlock@gmail.com>
2012-08-30 14:43:40 -0500

    Changed from group based to 'Authenticated' users for 'upload' page

--- ptmscout_web/ptmscout/__init__.py
+++ ptmscout_web/ptmscout/__init__.py
@@ -4,33 +4,37 @@ from sqlalchemy import engine_from_config
 from database.models import DBSession
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
-from database.user import checkUserGroup
+from database.user import checkUserGroup, getUserByRequest
 
 from pyramid.security import (
     Allow,
-    Everyone,
+    Authenticated,
     )
 
 class RootFactory(object):
-    __acl__ = [ (Allow, 'group:users', 'upload') ]
+    __acl__ = [ (Allow, Authenticated, 'upload') ]
     def __init__(self, request):
         pass
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    config = Configurator(settings=settings,
+                          root_factory='ptmscout.RootFactory')
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     
     authn_policy = AuthTktAuthenticationPolicy('$up3r$3cr3t', callback=checkUserGroup)
     authz_policy = ACLAuthorizationPolicy()
+    
+    config.set_request_property(getUserByRequest, 'user', reify=True)
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
     
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
+    config.add_route('upload', '/upload')
     config.add_route('process_login', '/process_login')
     config.add_route('register', '/register')
     config.add_route('process_registration', '/process_registration')
