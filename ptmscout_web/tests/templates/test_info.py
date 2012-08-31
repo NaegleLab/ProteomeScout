60f6a6f1c287501ff10584b487e56b8d411baa62
Matthew Matlock <matt.matlock@gmail.com>
2012-08-31 16:58:03 -0500

    Added functional tests for info views (about/terms)

---
+++ ptmscout_web/tests/templates/test_info.py
@@ -0,0 +1,21 @@
+import unittest
+from webtest.app import TestApp
+from ptmscout import main
+
+class InfoFunctionalTests(unittest.TestCase):
+    def setUp(self):
+        settings = { 'sqlalchemy.url': "mysql+mysqldb://ptmscout_web:ptmscout1@localhost:3306/ptmscout" }
+        app = main({}, **settings)
+        self.ptmscoutapp = TestApp(app)
+        
+    def tearDown(self):
+        from ptmscout.database import DBSession
+        del self.ptmscoutapp
+        DBSession.remove()
+
+    def test_about_renderer(self):
+        self.ptmscoutapp.get('/about', status=200)
+    
+    def test_terms_renderer(self):
+        self.ptmscoutapp.get('/terms', status=200)
+    
