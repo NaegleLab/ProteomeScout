c4653a191df36c89f255a4f306946266fdc43057
Matthew Matlock <matt.matlock@gmail.com>
2012-09-05 17:42:05 -0500

    Copied image assets from ptmscout_frontend, layout now points to new assets

--- ptmscout_web/ptmscout/config.py
+++ ptmscout_web/ptmscout/config.py
@@ -1,8 +1,6 @@
 homeUrl="../cgi-bin"
 documentationUrl="http://ptmscout.mit.edu/docs/index.php?"
-imagesUrl="../images"
 adminEmail = "matt.matlock@gmail.com"
-
 automailerEmail = "automailer@ptmscout.wustl.edu"
 
 MINIMUM_PASSWORD_LENGTH = 7
