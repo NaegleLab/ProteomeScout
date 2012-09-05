bfeb8a15e4de16056cd220ad546fd2fc62177af1
Matthew Matlock <matt.matlock@gmail.com>
2012-09-05 17:50:44 -0500

    Fixed import error

--- ptmscout_web/ptmscout/layout.py
+++ ptmscout_web/ptmscout/layout.py
@@ -2,13 +2,12 @@ from pyramid.renderers import get_renderer
 from pyramid.events import subscriber
 from pyramid.events import BeforeRender
 
-from config import homeUrl, documentationUrl, imagesUrl, adminEmail
+from config import homeUrl, documentationUrl, adminEmail
 
 @subscriber(BeforeRender)
 def add_path_definitions(event):
     event['homeUrl'] = homeUrl
     event['documentationUrl'] = documentationUrl
-    event['imagesUrl'] = imagesUrl
     event['adminEmail'] = adminEmail 
     event['layout'] =  site_layout()
 
