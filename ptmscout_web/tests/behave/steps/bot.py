ecf101de731b1843ac80673255970e55a7f52edb
Matthew Matlock <matt.matlock@gmail.com>
2012-09-11 00:32:05 -0500

    Implemented Behave tests for user account registration

--- ptmscout_web/tests/behave/steps/bot.py
+++ ptmscout_web/tests/behave/steps/bot.py
@@ -2,6 +2,7 @@ from ptmscout.database import user, experiment
 from mock import patch
 import re
 from ptmscout.database.permissions import Permission
+from ptmscout import strings
 
 class Bot(object):
     def __init__(self, app):
@@ -15,32 +16,35 @@ class Bot(object):
         self.institution = "The Institute of Turtles"
 
     def register_and_login(self):
-        self.register(self.app)
-        self.activate(self.app)
-        self.login(self.app)
+        self.register()
+        self.activate()
+        self.login()
 
     # this needs to change to a direct request for registration
     @patch('ptmscout.utils.mail.send_automail_message')
     def register(self, patch_mail):
         response = self.app.post('/process_registration', {'username':self.username, 'pass1':self.password, 'pass2':self.password, 'name':self.fullname, 'email':self.email, 'institution':self.institution}, status=200)
-        response.mustcontain("Registration Successful")
+        response.mustcontain(strings.user_registration_success_header)
+
+        patch_mail.assert_called_once()
+        self.activation_token = re.compile("&token=([a-z0-9]+)\">this link").search(patch_mail.call_args.__repr__()).group(1)
+        self.activation_link = re.compile("a href=\"(.*)\">this link").search(patch_mail.call_args.__repr__()).group(1)
 
         self.user = user.getUserByUsername(self.username)
-        self.activation_token = self.user.activation_token
         
     def activate(self):
         response = self.app.get('/activate_account', {'username':self.username, 'token':self.activation_token}, status=200)
-        response.mustcontain("Account Activation Succeeded")
+        response.mustcontain(strings.account_activation_success_header)
         
     def login(self):
         response = self.app.post('/process_login', {'username':self.username, 'password':self.password}, status=200)
-        response.mustcontain("You have successfully logged in.")
+        response.mustcontain(strings.login_page_success_message)
         
         self.user = user.getUserByUsername(self.username)
         
     def logout(self):
         response = self.app.get('/logout', status=200)
-        response.mustcontain("You have successfully logged out.")
+        response.mustcontain(strings.logout_page_message)
         
         self.user = None
         
--- ptmscout_web/tests/behave/user_accounts.feature
+++ ptmscout_web/tests/behave/user_accounts.feature
@@ -7,11 +7,11 @@ Feature: Private Data
 		Then I should be sent an e-mail on how to activate my account
 		
 	Scenario: Activate an account
-		Given that I have registered for an account
+		Given I have registered for an account
 		When I follow the link in my e-mail to activate my account
 		Then I should be able to log in to my account
 
 	Scenario: Forgot password	
-		Given I have forgotten my password
-		When I press "I forgot my password"
-		Then I should be sent a temporary token that I can follow to set a new password 
+		Given I have an existing account
+		When I press "I forgot my password" on the login page and enter my email address
+		Then I should be sent a temporary password to login
