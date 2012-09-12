d334727a76c37aa2441ebea8e499aaea093861ae
Matthew Matlock <matt.matlock@gmail.com>
2012-09-12 15:42:53 -0500

    Proteins now associated with GO terms, Accessions and Domains

--- ptmscout_web/ptmscout/database/protein.py
+++ ptmscout_web/ptmscout/database/protein.py
@@ -1,7 +1,42 @@
 from ptmscout.database import Base, DBSession
-from sqlalchemy.schema import Column
-from sqlalchemy.types import Integer, TEXT, VARCHAR
+from sqlalchemy.schema import Column, ForeignKey, UniqueConstraint, Table
+from sqlalchemy.types import Integer, TEXT, VARCHAR, Enum, Text, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import null
 
+go_association_table = Table('protein_GO', Base.metadata,
+    Column('protein_id', Integer(10), ForeignKey('protein.id')),
+    Column('GO_id', Integer(10), ForeignKey('GO.id')),
+    Column('version', VARCHAR(10))
+)
+
+class GeneOntology(Base):
+    __tablename__='GO'
+    id = Column(Integer(10), primary_key=True, autoincrement=True)
+    aspect = Column('aspect', Enum(['F','P','C']), default=null)
+    GO = Column(VARCHAR(10))
+    term = Column(Text)
+    version = Column(VARCHAR(10), default='0')
+    UniqueConstraint('aspect', 'GO', name="uniqueEntry")
+
+class Accession(Base):
+    __tablename__='acc'
+    id = Column(Integer(10), primary_key=True, autoincrement=True)
+    type = Column(VARCHAR(30))
+    value = Column(VARCHAR(45))
+    protein_id = Column(Integer(10), ForeignKey('protein.id'))
+    
+class Domain(Base):    
+    __tablename__='domain'
+    id = Column(Integer(10), primary_key=True, autoincrement=True)
+    label = Column(VARCHAR(45))
+    start = Column(Integer(10))
+    stop = Column(Integer(10))
+    p_value = Column(Float)
+    source = Column(Text, default='pfam')
+    params = Column(VARCHAR(45))
+    protein_id = Column(Integer(10), ForeignKey('protein.id'))
+    version = Column(Integer(11))
 
 class Protein(Base):
     __tablename__='protein'
@@ -12,7 +47,12 @@ class Protein(Base):
     name = Column(VARCHAR(100))
     date = Column(VARCHAR(7))
     
+    accessions = relationship("Accession")
+    GO_terms = relationship("GeneOntology", secondary=go_association_table)
+    domains = relationship("Domain")
+    
     def __init__(self):
+        print "Got protein: ", id
         pass
     
     def saveProtein(self):
--- ptmscout_web/tests/database/test_protein.py
+++ ptmscout_web/tests/database/test_protein.py
@@ -1,6 +1,7 @@
 from tests.DBTestCase import DBTestCase
 from ptmscout.database import protein
-from ptmscout.database.protein import NoSuchProtein
+from ptmscout.database.protein import NoSuchProtein, GeneOntology
+from sqlalchemy.exc import IntegrityError
 
 class ProteinTest(DBTestCase):
     def test_protein_should_be_gettable(self):
@@ -24,4 +25,57 @@ class ProteinTest(DBTestCase):
         else:
             self.fail("Expected exception: NoSuchProtein")
         
+    def test_proteins_should_be_associated_with_correct_accession(self):
+        prot = protein.getProteinById(1)
+        
+        self.assertEqual('USP24', prot.acc_gene)
+        
+        acc_ids = set([ acc.value for acc in prot.accessions ])
+        self.assertEqual(
+                set(['IPI00398505','NP_056121','Q9UPU5','IPI00902614', 'KIAA1057']), 
+                acc_ids)
+        
+    def test_proteins_should_be_associated_with_correct_GO_terms(self):
+        prot = protein.getProteinById(100)
+        
+        self.assertEqual('Mpv17l2', prot.acc_gene)
+        
+        acc_ids = set([ term.term for term in prot.GO_terms ])
+        self.assertEqual(
+                set(['mitochondrion','molecular_function','biological_process']), 
+                acc_ids)
+
+    def test_GO_terms_must_be_unique(self):
+        prot = protein.getProteinById(1)
+        
+        go_term1 = GeneOntology()
+        go_term1.aspect = 'F'
+        go_term1.GO = 'GO:unique'
+        go_term1.term = "SomeTermOfGO"
+        go_term2 = GeneOntology()
+        go_term2.aspect = 'F'
+        go_term2.GO = 'GO:unique'
+        go_term2.term = "SomeTermOfGO"
+        
+        prot.GO_terms.append(go_term1)
+        prot.GO_terms.append(go_term2)
+        prot.saveProtein()
+        
+        try:
+            self.session.flush()
+        except IntegrityError:
+            pass
+        except Exception, e:
+            self.fail("Unexpected exception occurred: " + str(e))
+        else:
+            self.fail("Expected IntegrityError on last DB operation")
+            
+        
+    def test_proteins_should_be_associated_with_correct_domains(self):
+        prot = protein.getProteinById(100)
+        
+        self.assertEqual('Mpv17l2', prot.acc_gene)
+        
+        domains = set([ d.label for d in prot.domains ])
+        self.assertEqual( set(['Mpv17_PMP22']), domains )
         
