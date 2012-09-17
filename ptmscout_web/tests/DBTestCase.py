b0fb5f3dd158660a1d32d43faf814a04d593f04e
Matthew Matlock <matt.matlock@gmail.com>
2012-09-17 15:21:56 -0500

    Made pathing in DBTestcase absolute

--- ptmscout_web/tests/DBTestCase.py
+++ ptmscout_web/tests/DBTestCase.py
@@ -6,8 +6,7 @@ from sqlalchemy import engine_from_config
 from ptmscout.database import DBSession, Base  # base declarative object
 
 import os
-here = os.path.dirname(__file__)
-settings = appconfig('config:' + os.path.join(here, '../', 'development.ini'))
+settings = appconfig('config:/' + os.path.join('data','ptmscout','ptmscout_web', 'development.ini'))
 
 class DBTestCase(unittest.TestCase):
     @classmethod
