a7cc3c369826dabe6cc2737f92435ad854e5735d
Matthew Matlock <matt.matlock@gmail.com>
2012-09-17 18:37:14 -0500

    Added urls for external accension refs

--- ptmscout_web/ptmscout/config.py
+++ ptmscout_web/ptmscout/config.py
@@ -5,3 +5,10 @@ automailerEmail = "automailer@ptmscout.wustl.edu"
 pubmedUrl = "www.ncbi.nlm.nih.gov/pubmed/%d"
 
 MINIMUM_PASSWORD_LENGTH = 7
+
+accession_urls = {'swissprot':"http://ca.expasy.org/uniprot/%s",
+                  'entrez_protein':"http://www.ncbi.nlm.nih.gov/protein/%s",
+                  'gi':"http://www.ncbi.nlm.nih.gov/entrez/viewer.fcgi?db=protein&amp;id=%s",
+                  'refseq':"http://www.ncbi.nlm.nih.gov/entrez/viewer.fcgi?db=protein&amp;id=%s",
+                  'GO':"http://amigo.geneontology.org/cgi-bin/amigo/term-details.cgi?term=%s",
+                  'genbank':"http://www.ncbi.nlm.nih.gov/entrez/viewer.fcgi?db=protein&amp;id=%s"}
