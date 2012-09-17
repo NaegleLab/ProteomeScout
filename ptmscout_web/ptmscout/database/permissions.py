b5c0530c2172994d86abe7659f76fac9119b8dc6
Matthew Matlock <matt.matlock@gmail.com>
2012-09-17 14:46:03 -0500

    Added invitations database ORM

--- ptmscout_web/ptmscout/database/permissions.py
+++ ptmscout_web/ptmscout/database/permissions.py
@@ -1,6 +1,6 @@
-from ptmscout.database import DBSession, Base
+from ptmscout.database import Base, DBSession
 from sqlalchemy.schema import Column, ForeignKey
-from sqlalchemy.types import Integer, Enum
+from sqlalchemy.types import Integer, Enum, VARCHAR
 from sqlalchemy.orm import relationship
 
 class Permission(Base):
@@ -14,3 +14,23 @@ class Permission(Base):
     def __init__(self, experiment, access_level='view'):
         self.experiment = experiment
         self.access_level = access_level
+
+class Invitation(Base):
+    __tablename__ = "invitations"
+    invited_email = Column(VARCHAR(50), primary_key=True)
+    experiment_id = Column(Integer(10), ForeignKey('experiment.id'), primary_key=True)
+    inviting_user_id = Column(Integer(10), ForeignKey('users.id'))
+
+    experiment = relationship("Experiment")
+    
+    def __init__(self, invited_email, experiment_id, inviting_user_id):
+        self.invited_email = invited_email
+        self.experiment_id = experiment_id
+        self.inviting_user_id = inviting_user_id
+        
+    def saveInvitation(self):
+        DBSession.add(self)
+
+
+def getInvitationsForUser(email):
+    return DBSession.query(Invitation).filter_by(invited_email=email).all()
--- ptmscout_web/tests/database/test_permissions.py
+++ ptmscout_web/tests/database/test_permissions.py
@@ -16,3 +16,18 @@ class PermissionsTestCase(DBTestCase):
         u.saveUser()
         
         self.assertEqual(u.id, experiment.getExperimentById(1, u).permissions[0].user.id)
+        
+    def test_getInvitationsForUser_should_return_correct_invites(self):
+        inviter = user.User("user","name","email","inst")
+        inviter.createUser("password")
+        inviter.saveUser() 
+        
+        i1 = permissions.Invitation("invited_user@institute.edu", 1, inviter.id)
+        i2 = permissions.Invitation("invited_user@institute.edu", 26, inviter.id)
+        i3 = permissions.Invitation("invited_user@institute.edu", 28, inviter.id)
+        
+        [ invite.saveInvitation() for invite in [i1,i2,i3] ]
+        
+        invitation_list = permissions.getInvitationsForUser("invited_user@institute.edu")
+        invited_exp_ids = [ invite.experiment_id for invite in invitation_list ]
+        self.assertEqual([1,26,28], invited_exp_ids)
