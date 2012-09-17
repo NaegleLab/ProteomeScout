b9963d7a30bd859c65effc882bafae8042ab410a
Matthew Matlock <matt.matlock@gmail.com>
2012-09-17 18:42:32 -0500

    Added translator between accession_types and human readables

--- ptmscout_web/ptmscout/strings.py
+++ ptmscout_web/ptmscout/strings.py
@@ -1,6 +1,11 @@
 email_regex = "[a-z0-9\.\-\_]+@[a-z0-9\.\-\_]+\.([a-z]+)$"
 
-
+accession_type_strings = {'ipi':"International Protein Index", 
+                     'gene_synonym':"Gene Synonym",
+                     'refseq': "RefSeq",
+                     'swissprot': "SwissProt",
+                     'genbank': "GenBank",
+                     'gi': "Gene Index"}
 
 success_header = "Success"
 
@@ -56,6 +61,8 @@ privatize_experiment_confirm_message = "Are you sure you want to make this data 
 privatize_experiment_success_message = "You have successfully made this experiment private."
 privatize_experiment_already_message = "Experiment has already been made private."
 
+protein_modification_sites_page_title = "Protein Modification Sites"
+
 share_experiment_page_title = "Share Experiment"
 
 upload_page_title = "Upload"
@@ -107,15 +114,3 @@ failure_reason_email_not_valid = "Email address is invalid"
 failure_reason_email_not_academic = "Email address must belong to .edu domain"
 failure_reason_password_too_short = "Password must be at least %d characters in length"
 failure_reason_email_address_not_on_record = "E-mail address does not match any user record"
-
-
-
-
-
-    
-
-
-
-
-
-
