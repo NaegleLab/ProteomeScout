bac49e884cc1258d7db182820e96f1b21877def7
Matthew Matlock <matt.matlock@gmail.com>
2012-09-17 15:23:31 -0500

    Added route for invite confirmation page

--- ptmscout_web/ptmscout/__init__.py
+++ ptmscout_web/ptmscout/__init__.py
@@ -44,6 +44,7 @@ def add_views(config):
     
     config.add_route('account_management', '/account')
     config.add_route('my_experiments', '/account/experiments')
+    config.add_route('invite_experiment', '/account/experiments/{id}/invite')
     config.add_route('share_experiment', '/account/experiments/{id}/share')
     config.add_route('publish_experiment', '/account/experiments/{id}/publish')
     config.add_route('privatize_experiment', '/account/experiments/{id}/unpublish')
