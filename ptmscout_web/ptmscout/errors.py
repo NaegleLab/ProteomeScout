e7c00694f967b8485e6055d52118f7861c925897
Matthew Matlock <matt.matlock@gmail.com>
2012-09-28 20:00:25 -0500

    Added error screen when attempting view non-existant experiment

--- ptmscout_web/ptmscout/errors.py
+++ ptmscout_web/ptmscout/errors.py
@@ -1,7 +1,8 @@
 from pyramid.view import forbidden_view_config, view_config
 from smtplib import SMTPRecipientsRefused
 from sqlalchemy.exc import IntegrityError
-from ptmscout.database.experiment import ExperimentAccessForbidden
+from ptmscout.database.experiment import ExperimentAccessForbidden,\
+    NoSuchExperiment
 from pyramid.httpexceptions import HTTPForbidden
 
 @view_config(context=IntegrityError, renderer='templates/information.pt')
@@ -17,6 +18,10 @@ def internal_mailer_error_view(exc, request):
             'header':"Mailer Error",
             'message':"An error was encountered while processing outgoing SMTP messages, please try again later.",
             'redirect': request.application_url + "/experiments"}
+
+@view_config(context=NoSuchExperiment, renderer='templates/forbidden.pt')
+def redirect_no_such_experiment(exc, request):
+    return {'pageTitle': "Forbidden"}
 
 @view_config(context=ExperimentAccessForbidden, renderer='templates/forbidden.pt')
 def redirect_forbidden(request):
