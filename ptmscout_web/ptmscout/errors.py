b9c3a2e3eda7c42672d094fef34a3b282011c64a
Matthew Matlock <matt.matlock@gmail.com>
2012-09-28 20:03:34 -0500

    Added friendly protein error view page

--- ptmscout_web/ptmscout/errors.py
+++ ptmscout_web/ptmscout/errors.py
@@ -4,6 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from ptmscout.database.experiment import ExperimentAccessForbidden,\
     NoSuchExperiment
 from pyramid.httpexceptions import HTTPForbidden
+from ptmscout.database.protein import NoSuchProtein
 
 @view_config(context=IntegrityError, renderer='templates/information.pt')
 def internal_database_error_view(exc, request):
@@ -18,6 +19,14 @@ def internal_mailer_error_view(exc, request):
             'header':"Mailer Error",
             'message':"An error was encountered while processing outgoing SMTP messages, please try again later.",
             'redirect': request.application_url + "/experiments"}
+
+@view_config(context=NoSuchProtein, renderer='templates/information.pt')
+def redirect_no_such_protein(exc, request):
+    return {'pageTitle':"Resource Error",
+            'header':"Resource Error",
+            'message':"No protein resource exists with the specified ID",
+            'redirect': request.application_url + "/proteins"}
+
 
 @view_config(context=NoSuchExperiment, renderer='templates/forbidden.pt')
 def redirect_no_such_experiment(exc, request):
