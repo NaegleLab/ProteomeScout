e307c4d6ed35152bce6ff5f2ba9377f36fed820f
Matthew Matlock <matt.matlock@gmail.com>
2012-10-02 10:39:40 -0500

    Moved view initialization function to views package

--- ptmscout_web/ptmscout/__init__.py
+++ ptmscout_web/ptmscout/__init__.py
@@ -4,6 +4,7 @@ from database import DBSession
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from database.user import getUserByRequest
+import views
 
 from pyramid.security import (
     Allow,
@@ -17,53 +18,8 @@ class RootFactory(object):
     def __init__(self, request):
         pass
     
-def add_views(config):
-    config.include('pyramid_mailer')
-    
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    
-    config.add_route('about', '/about')
-    config.add_route('terms', '/terms')
-    
-    config.add_route('redirect_to_experiments','/')
-    config.add_route('experiments','/experiments')
-    config.add_route('experiment','/experiments/{id}')
-    config.add_route('experiment_summary','/experiments/{id}/summary')
-    config.add_route('experiment_browse','/experiments/{id}/browse')
-    config.add_route('upload', '/upload')
-    
-    config.add_route('protein_data', '/proteins/{id}/data')
-    config.add_route('protein_GO', '/proteins/{id}/GO')
-    config.add_route('protein_expression', '/proteins/{id}/expression')
-    config.add_route('protein_mod_sites', '/proteins/{id}/modifications')
-    config.add_route('protein_main', '/proteins/{id}')
-    config.add_route('protein_search', '/proteins')
-    
-    config.add_route('login', '/login')
-    config.add_route('process_login', '/process_login')
-    config.add_route('logout', '/logout')
-    
-    config.add_route('register', '/register')
-    config.add_route('process_registration', '/process_registration')
-    config.add_route('activate_account', '/activate_account')
-    
-    config.add_route('forgot_password', '/forgot_password')
-    config.add_route('process_forgot_password', '/retrieve_password')
-    
-    config.add_route('account_management', '/account')
-    config.add_route('my_experiments', '/account/experiments')
-    config.add_route('invite_experiment', '/account/experiments/{id}/invite')
-    config.add_route('share_experiment', '/account/experiments/{id}/share')
-    config.add_route('publish_experiment', '/account/experiments/{id}/publish')
-    config.add_route('privatize_experiment', '/account/experiments/{id}/unpublish')
-    
-    config.add_route('change_password', '/change_password')
-    config.add_route('change_password_success', '/change_password_success')
-    
-    config.add_view(forbidden_view, context=Forbidden)
-    
 def includeme(config):
-    add_views(config)
+    views.add_views(config)
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -80,7 +36,7 @@ def main(global_config, **settings):
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
     
-    add_views(config)
+    views.add_views(config)
     
     config.scan()
     return config.make_wsgi_app()
---
+++ ptmscout_web/ptmscout/views/__init__.py
@@ -0,0 +1,47 @@
+from pyramid.exceptions import Forbidden
+from ptmscout.views.errors import forbidden_view
+
+def add_views(config):
+    config.include('pyramid_mailer')
+    
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    
+    config.add_route('about', '/about')
+    config.add_route('terms', '/terms')
+    
+    config.add_route('redirect_to_experiments','/')
+    config.add_route('experiments','/experiments')
+    config.add_route('experiment','/experiments/{id}')
+    config.add_route('experiment_summary','/experiments/{id}/summary')
+    config.add_route('experiment_browse','/experiments/{id}/browse')
+    config.add_route('upload', '/upload')
+    
+    config.add_route('protein_data', '/proteins/{id}/data')
+    config.add_route('protein_GO', '/proteins/{id}/GO')
+    config.add_route('protein_expression', '/proteins/{id}/expression')
+    config.add_route('protein_mod_sites', '/proteins/{id}/modifications')
+    config.add_route('protein_main', '/proteins/{id}')
+    config.add_route('protein_search', '/proteins')
+    
+    config.add_route('login', '/login')
+    config.add_route('process_login', '/process_login')
+    config.add_route('logout', '/logout')
+    
+    config.add_route('register', '/register')
+    config.add_route('process_registration', '/process_registration')
+    config.add_route('activate_account', '/activate_account')
+    
+    config.add_route('forgot_password', '/forgot_password')
+    config.add_route('process_forgot_password', '/retrieve_password')
+    
+    config.add_route('account_management', '/account')
+    config.add_route('my_experiments', '/account/experiments')
+    config.add_route('invite_experiment', '/account/experiments/{id}/invite')
+    config.add_route('share_experiment', '/account/experiments/{id}/share')
+    config.add_route('publish_experiment', '/account/experiments/{id}/publish')
+    config.add_route('privatize_experiment', '/account/experiments/{id}/unpublish')
+    
+    config.add_route('change_password', '/change_password')
+    config.add_route('change_password_success', '/change_password_success')
+    
+    config.add_view(forbidden_view, context=Forbidden)
