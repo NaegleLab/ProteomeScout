bfd26be4ec5702a6360f07672c36a5088d590bd0
Matthew Matlock <matt.matlock@gmail.com>
2012-10-02 10:24:51 -0500

    Refactored chart formatting to separate CSS

--- ptmscout_web/ptmscout/static/js/seqlogo.js
+++ ptmscout_web/ptmscout/static/js/seqlogo.js
@@ -17,17 +17,16 @@ function createSeqlogo(node, data, w, h){
 			.attr("class", "column")
 			.attr("transform", function(d, i) { return "translate({0},0)".format(i * column_width) })
 	
-			
 	entry = 
 		columns.selectAll("g.residue")
 				.data(function(d){ return d.f; })
 			.enter().append("g")
 				.attr("class", "residue")
-				.attr("transform", function(d, i) { return "translate(0, {0})scale(2.0,{1})".format( (d[2] / total * h) + 10 , d[1] / total * h / 20) });
-		
+				.attr("transform", function(d, i) { return "translate(0, {0})scale(2.0,{1})".format( (d[2] / total * h) + 10 , 1.04 * d[1] / total * h / 20) });
+	
 	entry.append("text")
 			.attr("x", 0)
-			.attr("y", 20)
+			.attr("y", 19)
 			.style("font-size", "30px")
 			.style("fill", function(d, i) { return colors(i) })
 			.text(function(d){ return d[0] });
