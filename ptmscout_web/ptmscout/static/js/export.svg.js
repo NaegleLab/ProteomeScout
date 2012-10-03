60d175119f3c5a8ef4181a65886ee123cd3ed631
Matthew Matlock <matt.matlock@gmail.com>
2012-10-03 16:28:35 -0500

    Fixed export to include graph CSS properties

--- ptmscout_web/ptmscout/static/js/export.svg.js
+++ ptmscout_web/ptmscout/static/js/export.svg.js
@@ -1,9 +1,28 @@
-function exportSVG(chart){
-	var id = "#" + chart.attr('id');
-	
+function displaySVG(id) {
 	var svgml = $(id).html();
     var b64 = Base64.encode(svgml);
     window.open("data:image/svg+xml;base64,\n"+b64)
+}
+
+function exportSVG(chart){
+	var id = "#" + chart.attr('id');
+	
+	if($(id + " svg style").size() == 0){
+		
+		css_url = $("#graph_css_export_url").text()
+		
+		$.get(css_url,
+			function(data) {
+				$(id + " svg").append("<style>" + data + "</style>");
+				displaySVG(id);
+			}
+		).error(function() {
+			alert("Request ERROR: unable to load stylesheet. Please try again.")
+		});
+		
+	} else {
+		displaySVG(id);
+	}
 }
 
 var export_num = 0;
@@ -15,7 +34,7 @@ function addExport(parent, container) {
 			.attr('version', "1.1")
 			.attr('xmlns', "http://www.w3.org/2000/svg");
 	
-	parent.attr('id', "chart" + export_num)
+	parent.attr('id', "chart" + export_num);
 	export_num++;
 	
 	container
