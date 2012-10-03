a632959bdbf2e5aca63d4485fcd1387effadb0ad
Matthew Matlock <matt.matlock@gmail.com>
2012-10-03 09:44:44 -0500

    Updated seq logo to get consistent cross-browser results

--- ptmscout_web/ptmscout/static/js/seqlogo.js
+++ ptmscout_web/ptmscout/static/js/seqlogo.js
@@ -6,7 +6,8 @@ function createSeqlogo(node, data, w, h){
 	
 	total = data.total;
 	total_columns = data.frequencies.length;
-	column_width = w / total_columns;
+	cw = w / total_columns;
+	ch = (h - 20) / 12;
 	
 	colors = d3.scale.category20();
 	
@@ -15,19 +16,21 @@ function createSeqlogo(node, data, w, h){
 			.data(data.frequencies)
 		.enter().append("g")
 			.attr("class", "column")
-			.attr("transform", function(d, i) { return "translate({0},0)".format(i * column_width) })
+			.attr("transform", function(d, i) { return "translate({0},0)".format(i * cw) })
 	
 	entry = 
 		columns.selectAll("g.residue")
 				.data(function(d){ return d.f; })
 			.enter().append("g")
 				.attr("class", "residue")
-				.attr("transform", function(d, i) { return "translate(0, {0})scale(2.0,{1})".format( (d[2] / total * h) + 10 , 1.04 * d[1] / total * h / 20) });
+				.attr("transform", function(d, i) { return "translate(0, {0})scale(1.0,{1})".format( (d[2] / total * h) + 10 , d[1] / total * h / ch) });
 	
-	entry.append("text")
-			.attr("x", 0)
-			.attr("y", 19)
-			.style("font-size", "30px")
-			.style("fill", function(d, i) { return colors(i) })
-			.text(function(d){ return d[0] });
+    entry.append("text")
+	    .attr("text-anchor", "middle")
+	    .attr("x", cw/2)
+	    .attr("y", ch - (ch/50))
+	    .attr("class", "arial")
+	    .style("font-size", 13.5 * (ch / 10) + "px")
+	    .style("fill", function(d, i) { return colors(i) })
+	    .text(function(d) { return d[0] });
 }
