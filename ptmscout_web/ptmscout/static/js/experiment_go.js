dd9d0efa3ebe72ca8cfb01a38c9d464fbc620bf0
Matthew Matlock <matt.matlock@gmail.com>
2012-10-09 12:16:13 -0500

    Added ability to expand and collapse GO tables

--- ptmscout_web/ptmscout/static/js/experiment_go.js
+++ ptmscout_web/ptmscout/static/js/experiment_go.js
@@ -4,5 +4,12 @@ $(document).ready(function(){
 	container = d3.select(".GO_map").append("div");
 	createGOMap(data, container);
 	
+	var max_row_display = 11;
+	
 	addExport(container, d3.select(".GO_map"));
+	
+	d3.selectAll(".GO_table")
+		.each(function(){
+			makeTableCollapsable(d3.select(this), 3, max_row_display);
+		});
 });
