e2c3b6936afc806081bf55b1ffc8273a2c89eea7
Matthew Matlock <matt.matlock@gmail.com>
2012-10-09 15:35:09 -0500

    Style updates. Needs a refactor. Done fiddling for now

--- ptmscout_web/ptmscout/static/js/go.map.js
+++ ptmscout_web/ptmscout/static/js/go.map.js
@@ -3,7 +3,7 @@ function compileNode(name, data){
 	for(i = 0; i < data.length; i++){
 		val+=data[i].value;
 	}
-	return {'GO':name, 'term':name, 'value':val, 'children':data};
+	return {'GO':name, 'term':"", 'value':val, 'children':data};
 }
 
 function createGOMap(json_data, container){
@@ -14,6 +14,15 @@ function createGOMap(json_data, container){
 	    y = d3.scale.linear().range([0, r]),
 	    node,
 	    root;
+	
+	var foreground_color = "#000";
+	var background_color = "#BBB";
+	var foreground_stroke = "none";
+	var background_stroke = "none";
+	
+	var view_limit = 60;
+	var id_size = 36;
+	var term_size = 24;
 	
 	var pack = d3.layout.pack()
 	    .size([r, r])
@@ -31,7 +40,7 @@ function createGOMap(json_data, container){
 	P_node = compileNode("Biological Process", json_data.P);
 	C_node = compileNode("Cellular Component", json_data.C);
 	
-	root = {'GO':"", 'term':"GO annotations", 'value':json_data.total, 'children':[F_node, P_node, C_node]};
+	root = {'GO':"", 'term':"", 'value':json_data.total, 'children':[F_node, P_node, C_node]};
 	
 	node = root;
 
@@ -45,18 +54,53 @@ function createGOMap(json_data, container){
     		.attr("cy", function(d) { return d.y; })
     		.attr("r", function(d) { return d.r; })
     		.on("click", function(d) { return zoom(node == d ? root : d); });
+	
+	var isNodeVisible = function(d, k) {
+		if(k===undefined)
+			k = 1;
+		return d.children.length != 1 && k * d.r > view_limit && k * d.r <= r / 2;
+	};
+	
+	var isNodeForeground = function(d, parent){
+		return d == parent || $.inArray(d, root.children) > -1;
+	};
+	
+	
+	var getScaledSize = function(d, size, scale) {
+		if(scale == undefined)
+			return 2 * d.r / r * size;
+		return (2 * (scale(d.y + d.r) - scale(d.y)) / r * size);
+	};
+	
+	vis.selectAll("text.id")
+    		.data(nodes.reverse())
+    	.enter()
+    		.append("svg:text")
+	    		.attr("class", function(d) { return "id " + (d.children ? "parent" : "child"); })
+	    		.attr("x", function(d) { return d.x; })
+	    		.attr("y", function(d) { return d.y; })
+	    		.attr("dy", ".35em")
+	    		.attr("text-anchor", "middle")
+	    		.style("fill", function(d) { return isNodeForeground(d) ? foreground_color : background_color})
+	    		.style("stroke", function(d) { return isNodeForeground(d) ? foreground_stroke : background_stroke})
+	    		.style("opacity", function(d) { return isNodeVisible(d) ? 1 : 0; })
+	    		.style("font-size", function(d) { return getScaledSize(d, id_size) + "px" })
+	    		.text(function(d) { return d.GO; });
 
-	vis.selectAll("text")
-    		.data(nodes)
-    	.enter().append("svg:text")
-    		.attr("class", function(d) { return d.children ? "parent" : "child"; })
-    		.attr("x", function(d) { return d.x; })
-    		.attr("y", function(d) { return d.y; })
-    		.attr("dy", ".35em")
-    		.attr("text-anchor", "middle")
-    		.style("opacity", function(d) { return d.children.length != 1 && d.r > 20 ? 1 : 0; })
-    		.style("font-size", function(d) { return d.r / 10 })
-    		.text(function(d) { return d.GO; });
+	vis.selectAll("text.term")
+			.data(nodes)
+		.enter()
+			.append("svg:text")
+				.attr("class", function(d) { return "term " + (d.children ? "parent" : "child"); })
+	    		.attr("x", function(d) { return d.x; })
+	    		.attr("y", function(d) { return d.y; })
+	    		.attr("dy", function(d) { return getScaledSize(d, id_size) })
+	    		.attr("text-anchor", "middle")
+	    		.style("fill", function(d) { return isNodeForeground(d) ? foreground_color : background_color})
+	    		.style("stroke", function(d) { return isNodeForeground(d) ? foreground_stroke : background_stroke})
+	    		.style("opacity", function(d) { return isNodeVisible(d) ? 1 : 0; })
+	    		.style("font-size", function(d) { return getScaledSize(d, term_size) + "px" })
+	    		.text(function(d) { return d.term; });
 
 	d3.select("body").on("click", function() { zoom(root); });
 	
@@ -65,7 +109,7 @@ function createGOMap(json_data, container){
 	  var k = r / d.r / 2;
 	  x.domain([d.x - d.r, d.x + d.r]);
 	  y.domain([d.y - d.r, d.y + d.r]);
-	
+	  var parent = d;
 	  var t = vis.transition()
 	      .duration(d3.event.altKey ? 7500 : 750);
 	
@@ -74,12 +118,24 @@ function createGOMap(json_data, container){
 	      .attr("cy", function(d) { return y(d.y); })
 	      .attr("r", function(d) { return k * d.r; });
 	
-	  t.selectAll("text")
+	  t.selectAll("text.id")
 	      .attr("x", function(d) { return x(d.x); })
 	      .attr("y", function(d) { return y(d.y); })
-	      .style("opacity", function(d) { return d.children.length != 1 && k * d.r > 20 && k * d.r <= r / 2 ? 1 : 0; })
-	      .style("font-size", function(d) { return (y(d.y + d.r) - y(d.y - d.r)) / 20 });
-	
+	      .style("fill", function(d) { return isNodeForeground(d, parent) ? foreground_color : background_color})
+	      .style("stroke", function(d) { return isNodeForeground(d, parent) ? foreground_stroke : background_stroke})
+	      .style("opacity", function(d) { return isNodeVisible(d, k) ? 1 : 0; })
+	      .style("font-size", function(d) { return getScaledSize(d, id_size, y) + "px" });
+	  
+	  t.selectAll("text.term")
+	      .attr("x", function(d) { return x(d.x); })
+	      .attr("y", function(d) { return y(d.y); })
+	      .attr("dy", function(d) { return getScaledSize(d, id_size, y); })
+	      .style("fill", function(d) { return isNodeForeground(d, parent) ? foreground_color : background_color})
+	      .style("stroke", function(d) { return isNodeForeground(d, parent) ? foreground_stroke : background_stroke})
+	      .style("opacity", function(d) { return isNodeVisible(d, k) ? 1 : 0; })
+	      .style("font-size", function(d) { return getScaledSize(d, term_size, y) + "px" });
+
+	  
 	  node = d;
 	  d3.event.stopPropagation();
 	}
