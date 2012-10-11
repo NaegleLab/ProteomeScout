8c6b8ade200f66ef4e447b65a52d1c51c4115586
Matthew Matlock <matt.matlock@gmail.com>
2012-10-11 15:25:39 -0500

    Enabled use of bots in integration tests

--- ptmscout_web/tests/PTMScoutTestCase.py
+++ ptmscout_web/tests/PTMScoutTestCase.py
@@ -2,12 +2,15 @@ import unittest
 from pyramid import testing
 from ptmscout import main
 from webtest.app import TestApp
+from tests.behave.steps.bot import Bot
 
 class IntegrationTestCase(unittest.TestCase):
     def setUp(self):
         settings = { 'sqlalchemy.url': "mysql+mysqldb://ptmscout_web:ptmscout1@localhost:3306/ptmscout_dev" }
         app = main({}, **settings)
         self.ptmscoutapp = TestApp(app)
+        self.bot = Bot(self.ptmscoutapp)
+        self.bot.register_and_login()
         
     def tearDown(self):
         from ptmscout.database import DBSession
