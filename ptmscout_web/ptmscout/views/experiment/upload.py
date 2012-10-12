401e3fb9f43c22f551eb515e8d0ff4f958fd9dff
Matthew Matlock <matt.matlock@gmail.com>
2012-10-12 18:23:31 -0500

    Implemented data file save, verification, and experiment DB entry creation.

--- ptmscout_web/ptmscout/views/experiment/upload.py
+++ ptmscout_web/ptmscout/views/experiment/upload.py
@@ -1,16 +1,118 @@
 from pyramid.view import view_config
-from ptmscout.views.layout import site_layout
-from ptmscout.config import strings
+from ptmscout.config import strings, settings
 import base64
 import json
 from ptmscout.utils import webutils
 from pyramid.httpexceptions import HTTPFound
+import time
+import os
+import datetime
+from ptmscout.database import experiment
 
-def create_experiment_and_start_upload(field_dict, tmp_file):
-    pass
+def create_experiment_and_start_upload(field_dict, exp_file, current_user):
+    if(field_dict['load_type'] == 'reload'):
+        nexp = experiment.getExperimentById(int(field_dict['parent_experiment']), current_user)
+    else: 
+        nexp = experiment.Experiment()
+        nexp.public = 0
+        nexp.experiment_id = None    
 
-def verify_datafile(request):
-    pass
+    nexp.name = field_dict['experiment_name']
+    nexp.description = field_dict['description']
+    
+    nexp.dataset = exp_file
+    nexp.ready = 0
+    
+    nexp.published = 1 if field_dict['published'] == "yes" else 0
+    nexp.ambiguity = 1 if field_dict['ambiguous'] == "yes" else 0
+    nexp.URL = field_dict['URL']
+    nexp.export = 1
+
+    nexp.contact = None
+    nexp.author = None
+    nexp.journal = None
+    nexp.volume = None
+    nexp.page_end = None
+    nexp.page_start = None
+    nexp.publication_month = None
+    nexp.publication_year = None
+    nexp.errorLog = None
+    nexp.PMID = None
+    
+    nexp.submitter_id = current_user.id
+    
+    if(field_dict['load_type'] == 'extension'):
+        nexp.experiment_id = int(field_dict['parent_experiment'])
+        
+    if(field_dict['published'] == "yes"):
+        nexp.contact = field_dict['author_contact']
+        nexp.author = field_dict['authors']
+        
+        nexp.volume = int(field_dict['volume'])
+        nexp.page_start = field_dict['page_start']
+        nexp.page_end = field_dict['page_end']
+        
+        nexp.publication_month = field_dict['publication_month']
+        nexp.publication_year = int(field_dict['publication_year'])
+        
+        nexp.journal = field_dict['journal']
+        if(field_dict['pmid'] != ""):
+            nexp.PMID = int(field_dict['pmid'])
+        
+
+    
+    nexp.saveExperiment()
+    return nexp.id
+
+def verify_datafile(exp_file):
+    infile = open(os.path.join(settings.experiment_data_file_path, exp_file), 'rb')
+    header = [ col.strip().lower() for col in infile.readline().strip().split("\t") ]
+    infile.close()
+    
+    acc_columns = []
+    pep_columns = []
+    
+    for col in header:
+        if col == "acc" or col.find("acc:") == 0:
+            acc_columns.append(col)
+        if col == "peptide" or col.find("pep:") == 0:
+            pep_columns.append(col)
+    
+    if len(acc_columns) == 0:
+        return strings.failure_reason_experiment_header_no_acc_column
+    if len(acc_columns) > 1:
+        return strings.failure_reason_experiment_header_multiple_acc_columns
+    
+    if len(pep_columns) == 0:
+        return strings.failure_reason_experiment_header_no_peptide_column
+    if len(pep_columns) > 1:
+        return strings.failure_reason_experiment_header_multiple_peptide_column
+    
+    return None
+
+def save_datafile(request):
+    exp_file = "experiment_data" + str(time.time())
+    
+    input_file = request.POST['data_file'].file
+    output_file = open(os.path.join(settings.experiment_data_file_path, exp_file), 'wb')
+    
+    input_file.seek(0)
+    while 1:
+        data = input_file.read(2<<16)
+        if not data:
+            break
+        output_file.write(data)
+    output_file.close()
+    
+    os.system("mac2unix %s" % os.path.join(settings.experiment_data_file_path, exp_file))
+    os.system("dos2unix %s" % os.path.join(settings.experiment_data_file_path, exp_file))
+    
+    error = verify_datafile(exp_file)
+    
+    if error == None:
+        return True, exp_file
+    else:
+        return False, error
 
 
 def get_enum_fields(request, users_experiments):
@@ -32,7 +134,6 @@ def get_required_fields(request):
                   'description', 
                   'experiment_name', 
                   'published', 
-                  'author_contact'
                   'ambiguous']
     
     if (webutils.post(request, 'load_type', "") != "new"):
@@ -42,6 +143,7 @@ def get_required_fields(request):
         req_fields.append('change_description')
         
     if (webutils.post(request, 'published', "") == "yes"):
+        req_fields.append('author_contact')
         req_fields.append('authors')
         req_fields.append('journal')
         req_fields.append('publication_year')
@@ -60,11 +162,24 @@ def check_required_fields(request, users_experiments):
                      'parent_experiment': "Parent Experiment", 
                      'published': "Published",
                      'ambiguous': "Possibly ambiguous accessions",
-                     'author_contact' : "Author Contact Email"}
+                     'author_contact' : "Author Contact Email",
+                     'page_start': "Page Start",
+                     'page_end': "Page End",
+                     'author_contact':"Author Contact",
+                     'authors': "Authors",
+                     'journal': "Journal",
+                     'volume': "Volume",
+                     'change_description': "Change Description",
+                     'load_type': "Load Type",
+                     'data_file': "Data File",
+                     'description': "Description",
+                     'experiment_name': "Experiment Name"}
     
     field_dict = {}
     for field in request.POST:
-        field_dict[field] = webutils.get(request, field, "").strip()
+        field_dict[field] = webutils.post(request, field, "")
+        if(isinstance(field_dict[field], str)):
+            field_dict[field] = field_dict[field].strip()
 
     if('terms_of_use' not in field_dict):
         return False, strings.failure_reason_terms_of_use_not_accepted, field_dict
@@ -72,8 +187,8 @@ def check_required_fields(request, users_experiments):
     del field_dict['terms_of_use']
     
     for field in req_fields:
-        if field_dict[field] == "":    
-            return False, strings.failure_reason_required_fields_cannot_be_empty, field_dict
+        if field_dict[field] == "":
+            return False, strings.failure_reason_required_fields_cannot_be_empty % field_name_dict[field], field_dict
         
     numeric_fields = get_numeric_fields(request)
     
@@ -120,16 +235,20 @@ def user_upload(request):
         if not passed:
             reason = error
         else:
-            passed, result = verify_datafile(request)
+            passed, result = save_datafile(request)
             
             if not passed:
                 reason = result
             else:
-                exp_id = create_experiment_and_start_upload(field_dict, result)
+                exp_id = create_experiment_and_start_upload(field_dict, result, request.user)
                 return HTTPFound(request.application_url + "/upload/" + str(exp_id))
     
+    if 'data_file' in field_dict:
+        del field_dict['data_file']
+    
+    dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime) else obj
     return {'pageTitle': strings.upload_page_title,
             'user_experiments': dict_exps,
-            'json_user_data': base64.b64encode(json.dumps(dict_exps)),
+            'json_user_data': base64.b64encode(json.dumps(dict_exps, default=dthandler)),
             'reason':reason,
             'formfields':base64.b64encode(json.dumps(field_dict))}
--- ptmscout_web/tests/views/experiment/test_upload_views.py
+++ ptmscout_web/tests/views/experiment/test_upload_views.py
@@ -1,14 +1,224 @@
 from tests.PTMScoutTestCase import IntegrationTestCase, UnitTestCase
 from pyramid.testing import DummyRequest
-from ptmscout.views.experiment.upload import user_upload, check_required_fields
-from ptmscout.config import strings
+from ptmscout.views.experiment.upload import user_upload, check_required_fields,\
+    verify_datafile, save_datafile, create_experiment_and_start_upload
+from ptmscout.config import strings, settings
 from tests.views.mocking import createMockUser, createMockPermission,\
     createMockExperiment
 import base64
 import json
 from mock import patch
+import os
+import cgi
 
 class TestUploadView(UnitTestCase):
+    
+    def create_field_dict(self):
+        field_dict = {}
+        field_dict['experiment_name'] = "some name for an exp"
+        field_dict['description'] = "Some description"
+        field_dict['load_type'] = "extension"
+        field_dict['published'] = "yes"
+        field_dict['author_contact'] = "someemail@institute.edu"
+        field_dict['ambiguous'] = "no"
+        
+        field_dict['parent_experiment'] = "24"
+        field_dict['change_description'] = "We did some stuff to remove some other stuff that wasn't all that great for our analysis" 
+        field_dict['authors'] = "Makalaster, David; Claypool, Les; Brigade, Frog"
+        field_dict['journal'] = "Journal of serendipitous results"
+        field_dict['publication_year'] = "2008"
+        field_dict['publication_month'] = "may"
+        field_dict['volume'] = "102"
+        field_dict['page_start'] = "D102"
+        field_dict['page_end'] = ""
+        field_dict['pmid'] = "1234"
+        field_dict['URL'] = ""
+        return field_dict
+    
+    @patch('ptmscout.database.experiment.getExperimentById')
+    def test_create_experiment_and_start_upload_should_fetch_existing_experiment_for_reload(self, patch_getExperiment):
+        field_dict = self.create_field_dict()
+        field_dict['load_type'] = 'reload'
+        exp_file = "tmpfile1002344.tmp"
+        
+        current_user = createMockUser("username", "email", "password", "1")
+        
+        experiment_instance = createMockExperiment(int(field_dict['parent_experiment']), 0, 0)
+        patch_getExperiment.return_value = experiment_instance 
+        
+        result = create_experiment_and_start_upload(field_dict, exp_file, current_user)
+        
+        patch_getExperiment.assert_called_once_with(int(field_dict['parent_experiment']), current_user)
+        experiment_instance.saveExperiment.assert_called_once_with()
+        
+        self.assertEqual(0, experiment_instance.ready)
+        self.assertEqual(experiment_instance.id, result)
+        self.assertEqual(field_dict['experiment_name'], experiment_instance.name)
+        self.assertEqual(field_dict['author_contact'], experiment_instance.contact)
+        self.assertEqual(field_dict['authors'], experiment_instance.author)
+        self.assertEqual(field_dict['journal'], experiment_instance.journal)
+        self.assertEqual(1, experiment_instance.published)
+        self.assertEqual(0, experiment_instance.ambiguity)
+        self.assertEqual(0, experiment_instance.public)
+        self.assertEqual(1, experiment_instance.export)
+        self.assertEqual(int(field_dict['publication_year']), experiment_instance.publication_year)
+        self.assertEqual(field_dict['publication_month'], experiment_instance.publication_month)
+        self.assertEqual(int(field_dict['volume']), experiment_instance.volume)
+        self.assertEqual(field_dict['page_start'], experiment_instance.page_start)
+        self.assertEqual(field_dict['page_end'], experiment_instance.page_end)
+        self.assertEqual(int(field_dict['pmid']), experiment_instance.PMID)
+        self.assertEqual(field_dict['URL'], experiment_instance.URL)
+        
+        self.assertEqual(exp_file, experiment_instance.dataset)
+        
+        self.assertEqual(current_user.id, experiment_instance.submitter_id)
+    
+    @patch('ptmscout.database.experiment.Experiment')
+    def test_create_experiment_and_start_upload_should_leave_fields_blank_when_not_needed(self, patch_experiment):
+        field_dict = self.create_field_dict()
+        field_dict['load_type'] = 'new'
+        field_dict['published'] = 'no'
+        exp_file = "tmpfile1002344.tmp"
+        
+        current_user = createMockUser("username", "email", "password", "1")
+        result = create_experiment_and_start_upload(field_dict, exp_file, current_user)
+        
+        experiment_instance = patch_experiment.return_value
+        experiment_instance.saveExperiment.assert_called_once_with()
+        
+        self.assertEqual(0, experiment_instance.ready)
+        self.assertEqual(experiment_instance.id, result)
+        self.assertEqual(field_dict['experiment_name'], experiment_instance.name)
+        self.assertEqual(None, experiment_instance.contact)
+        self.assertEqual(None, experiment_instance.author)
+        self.assertEqual(None, experiment_instance.journal)
+        self.assertEqual(0, experiment_instance.published)
+        self.assertEqual(0, experiment_instance.ambiguity)
+        self.assertEqual(0, experiment_instance.public)
+        self.assertEqual(1, experiment_instance.export)
+        self.assertEqual(None, experiment_instance.publication_year)
+        self.assertEqual(None, experiment_instance.publication_month)
+        self.assertEqual(None, experiment_instance.volume)
+        self.assertEqual(None, experiment_instance.page_start)
+        self.assertEqual(None, experiment_instance.page_end)
+        self.assertEqual(None, experiment_instance.PMID)
+        self.assertEqual("", experiment_instance.URL)
+        self.assertEqual(None, experiment_instance.experiment_id)
+        
+        self.assertEqual(exp_file, experiment_instance.dataset)
+        self.assertEqual(current_user.id, experiment_instance.submitter_id)
+        
+    @patch('ptmscout.database.experiment.Experiment')
+    def test_create_experiment_and_start_upload_should_spawn_job(self, patch_experiment):
+        field_dict = self.create_field_dict()
+        exp_file = "tmpfile1002344.tmp"
+        
+        current_user = createMockUser("username", "email", "password", "1")
+        result = create_experiment_and_start_upload(field_dict, exp_file, current_user)
+        
+        experiment_instance = patch_experiment.return_value
+        experiment_instance.saveExperiment.assert_called_once_with()
+        
+        self.assertEqual(0, experiment_instance.ready)
+        self.assertEqual(experiment_instance.id, result)
+        self.assertEqual(field_dict['experiment_name'], experiment_instance.name)
+        self.assertEqual(field_dict['author_contact'], experiment_instance.contact)
+        self.assertEqual(field_dict['authors'], experiment_instance.author)
+        self.assertEqual(field_dict['journal'], experiment_instance.journal)
+        self.assertEqual(1, experiment_instance.published)
+        self.assertEqual(0, experiment_instance.ambiguity)
+        self.assertEqual(0, experiment_instance.public)
+        self.assertEqual(1, experiment_instance.export)
+        self.assertEqual(int(field_dict['publication_year']), experiment_instance.publication_year)
+        self.assertEqual(field_dict['publication_month'], experiment_instance.publication_month)
+        self.assertEqual(int(field_dict['volume']), experiment_instance.volume)
+        self.assertEqual(field_dict['page_start'], experiment_instance.page_start)
+        self.assertEqual(field_dict['page_end'], experiment_instance.page_end)
+        self.assertEqual(int(field_dict['pmid']), experiment_instance.PMID)
+        self.assertEqual(field_dict['URL'], experiment_instance.URL)
+        self.assertEqual(int(field_dict['parent_experiment']), experiment_instance.experiment_id)
+        
+        self.assertEqual(exp_file, experiment_instance.dataset)
+        
+        self.assertEqual(current_user.id, experiment_instance.submitter_id)
+        
+    
+    def exec_test_verify(self, filename, tmp_file):
+        cwd = settings.ptmscout_path
+        
+        os.chdir(cwd)
+        os.system("mac2unix -n %s %s" % (os.path.join("tests", "behave", "data", filename), os.path.join(settings.experiment_data_file_path, tmp_file)))
+        os.system("dos2unix %s" % (os.path.join(settings.experiment_data_file_path, tmp_file)))
+        
+        error = verify_datafile(tmp_file)
+        
+        os.remove(os.path.join(settings.experiment_data_file_path, tmp_file))
+        
+        return error
+    
+    def test_verify_datafile_should_fail_on_no_acc(self):
+        filename = "datasetLoad_noAcc.txt"
+        tmp_file = "tmp_test.conv"
+        
+        error = self.exec_test_verify(filename, tmp_file)
+        
+        self.assertEqual(strings.failure_reason_experiment_header_no_acc_column, error)
+    
+    def test_verify_datafile_should_fail_on_multiple_peptide(self):
+        filename = "datasetLoad_moreThanOnePep.txt"
+        tmp_file = "tmp_test.conv"
+        
+        error = self.exec_test_verify(filename, tmp_file)
+        
+        self.assertEqual(strings.failure_reason_experiment_header_multiple_peptide_column, error)
+    
+    def test_verify_datafile_should_pass_on_good_header(self):
+        filename = "datasetLoad_correctDataset.txt"
+        tmp_file = "tmp_test.conv"
+        
+        error = self.exec_test_verify(filename, tmp_file)
+
+        self.assertEqual(None, error)
+
+    @patch('ptmscout.views.experiment.upload.verify_datafile')
+    def test_save_datafile_should_save_experiment_datafile_and_pass(self, patch_verify):
+        request = DummyRequest()
+        request.POST['data_file'] = cgi.FieldStorage()
+        
+        cwd = settings.ptmscout_path
+        os.chdir(cwd)
+        
+        filename = "datasetLoad_correctDataset.txt"
+        request.POST['data_file'].filename = filename
+        request.POST['data_file'].file = open(os.sep.join(["tests","behave","data",filename]), 'rb')
+        
+        patch_verify.return_value = None
+        
+        status, exp_file = save_datafile(request)
+        
+        os.chdir(os.sep.join(["tests", "behave", "data"]))
+        os.system("mac2unix -n %s tmp_test.conv" % (filename))
+        os.system("dos2unix tmp_test.conv")
+
+        tf = open("tmp_test.conv", 'rb')
+        test_file = tf.read()
+        tf.close()
+        
+        os.remove("tmp_test.conv")
+        os.chdir(cwd)
+        
+        os.chdir(settings.experiment_data_file_path)
+        tf = open(exp_file, 'rb')
+        result_file = tf.read()
+        tf.close()
+        
+        os.remove(exp_file)
+        
+        patch_verify.assert_called_once_with(exp_file)
+        
+        self.assertEqual(True, status)
+        self.assertEqual(test_file, result_file)
+
 
     @patch('ptmscout.views.experiment.upload.get_enum_fields')
     @patch('ptmscout.views.experiment.upload.get_numeric_fields')
@@ -78,7 +288,7 @@ class TestUploadView(UnitTestCase):
         patch_required.assert_called_once_with(request)
         
         self.assertEqual(False, status)
-        self.assertEqual(strings.failure_reason_required_fields_cannot_be_empty, error)
+        self.assertEqual(strings.failure_reason_required_fields_cannot_be_empty % "Published", error)
         self.assertEqual({'pmid':"1234", 'URL':"http://somestuff.com", 'published':""}, field_dict)
 
     def test_check_required_fields_should_return_false_if_terms_not_accepted(self):
@@ -123,7 +333,7 @@ class TestUploadView(UnitTestCase):
         
 
     @patch('ptmscout.views.experiment.upload.create_experiment_and_start_upload')
-    @patch('ptmscout.views.experiment.upload.verify_datafile')
+    @patch('ptmscout.views.experiment.upload.save_datafile')
     @patch('ptmscout.views.experiment.upload.check_required_fields')
     def test_view_should_start_upload_on_successful_verification(self, patch_check, patch_verify, patch_start):
         request = DummyRequest()
@@ -143,10 +353,10 @@ class TestUploadView(UnitTestCase):
         
         patch_check.assert_called_once_with(request, [])
         patch_verify.assert_called_once_with(request)
-        patch_start.assert_called_once_with(field_dict, tmp_filename)
+        patch_start.assert_called_once_with(field_dict, tmp_filename, request.user)
         
 
-    @patch('ptmscout.views.experiment.upload.verify_datafile')
+    @patch('ptmscout.views.experiment.upload.save_datafile')
     @patch('ptmscout.views.experiment.upload.check_required_fields')
     def test_view_should_handle_form_submission_fail_on_datafile_failure(self, patch_check, patch_verify):
         request = DummyRequest()
