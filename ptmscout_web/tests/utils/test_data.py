1219291d1847c97399eafe745b4ca7b94d87f4d0
Matthew Matlock <matt.matlock@gmail.com>
2012-10-12 21:00:38 -0500

    Added stub for testing data module

---
+++ ptmscout_web/tests/utils/test_data.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestCase(unittest.TestCase):
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+    
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        
+    def test_start_upload(self):
+        pass
