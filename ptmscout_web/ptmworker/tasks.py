51c7da8ea67f2be02d5522a81dcdfaee34f474f7
Matthew Matlock <matt.matlock@gmail.com>
2012-10-16 14:35:04 -0500

    stubbed import tasks

--- ptmscout_web/ptmworker/tasks.py
+++ ptmscout_web/ptmworker/tasks.py
@@ -1,2 +1,19 @@
 from celery.task import task
+from ptmscout.config import settings
+import os
 
+@task 
+def load_protein(accession):
+    pass
+
+@task
+def load_peptide(protein, pep_seq):
+    pass
+
+@task
+def insert_run_data(protein_id, peptide_id, run_data):
+    pass
+
+@task
+def start_import(exp, column_map={}):
+    open(os.path.join(settings.experiment_data_file_path, exp.dataset), 'rb')
