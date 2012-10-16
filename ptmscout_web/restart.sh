824d87b1eedac25a0db824ee0dde1fd39331781f
Matthew Matlock <matt.matlock@gmail.com>
2012-10-16 12:27:23 -0500

    Restart scripts kills and restarts celery workers

--- ptmscout_web/restart.sh
+++ ptmscout_web/restart.sh
@@ -3,4 +3,9 @@
 sudo chmod -R 775 *
 sudo chown -R www-data *
 sudo chgrp -R development *
+
+kill `cat "logs/pids"`
+/data/pyramid/bin/pceleryd development.ini --logfile=logs/pceleryd --loglevel=DEBUG --pidfile=logs/pids > /dev/null &
+
 sudo apache2ctl restart
+
