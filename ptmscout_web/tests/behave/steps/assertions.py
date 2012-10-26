e17dff7cce45b46cc9fef589871fb45f26ddd9ad
Matthew Matlock <matt.matlock@gmail.com>
2012-10-26 00:09:19 -0500

    Added helper for assertions

--- ptmscout_web/tests/behave/steps/assertions.py
+++ ptmscout_web/tests/behave/steps/assertions.py
@@ -3,3 +3,7 @@ def assertDoesNotContain(needle, haystack):
     
 def assertContains(needle, haystack):
     assert (haystack.find(needle) > -1)
+    
+def assertEqual(expected, actual):
+    if actual != expected:
+        raise AssertionError("%s != %s" % (str(expected), str(actual)))
