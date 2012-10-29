bbf374e6b9b0b4c7bc06bcc2da2f0afb1c05f0e5
Matthew Matlock <matt.matlock@gmail.com>
2012-10-29 14:39:44 -0500

    Added helper function to accumulate exceptions

--- ptmscout_web/ptmscout/utils/webutils.py
+++ ptmscout_web/ptmscout/utils/webutils.py
@@ -14,3 +14,10 @@ def __check_array(array, var, default):
         pass
      
     return default
+
+
+def call_catch(errors, method, *args, **kwargs):
+    try:
+        method(args, kwargs)
+    except Exception, e:
+        errors.append(e)
