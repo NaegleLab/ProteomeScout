bb944570c19d5c33b9f2e2223f806350c34f0010
Matthew Matlock <matt.matlock@gmail.com>
2012-10-30 14:57:49 -0500

    Added logging for error exceptions

--- ptmscout_web/ptmscout/views/errors.py
+++ ptmscout_web/ptmscout/views/errors.py
@@ -6,9 +6,13 @@ from ptmscout.database.experiment import ExperimentAccessForbidden,\
 from ptmscout.database.protein import NoSuchProtein
 from ptmscout.config import strings
 from ptmscout.database.upload import NoSuchSession, SessionAccessForbidden
+import logging
 
 @view_config(context=IntegrityError, renderer='ptmscout:templates/info/information.pt')
 def internal_database_error_view(exc, request):
+    log = logging.getLogger('ptmscout')
+    log.exception(exc)
+    
     return {'pageTitle':"Database Error",
             'header':"Database Error",
             'message':"There was an internal database error during the processing of your request, please try again later.",
@@ -16,6 +20,9 @@ def internal_database_error_view(exc, request):
 
 @view_config(context=SMTPRecipientsRefused, renderer='ptmscout:templates/info/information.pt')
 def internal_mailer_error_view(exc, request):
+    log = logging.getLogger('ptmscout')
+    log.exception(exc)
+    
     return {'pageTitle':"Mailer Error",
             'header':"Mailer Error",
             'message':"An error was encountered while processing outgoing SMTP messages, please try again later.",
@@ -45,10 +52,6 @@ def experiment_not_available(exc, request):
     return {'pageTitle':strings.error_resource_not_ready_page_title,
             'header':strings.error_resource_not_ready_page_title,
             'message':strings.error_resource_not_ready_message % (request.application_url + "/account/experiments")}
-
-@view_config(context=NoSuchExperiment, renderer='ptmscout:templates/info/forbidden.pt')
-def redirect_no_such_experiment(exc, request):
-    return {'pageTitle': "Forbidden"}
 
 @view_config(context=ExperimentAccessForbidden, renderer='ptmscout:templates/info/forbidden.pt')
 def redirect_forbidden(request):
