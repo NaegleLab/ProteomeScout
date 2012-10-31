6559911e9a7ba62d228336a5fb1cec84326ec326
Matthew Matlock <matt.matlock@gmail.com>
2012-10-30 19:16:53 -0500

    Added javascript to query pubmed citation webservice

--- ptmscout_web/ptmscout/static/js/upload.form.js
+++ ptmscout_web/ptmscout/static/js/upload.form.js
@@ -36,4 +36,47 @@ $(document).ready(function(){
 		});
 	
 	$('#new_dataset').click()
+	
+	month_vals = ['january','february','march','april','may','june','july','august','september','october','november','december']
+	
+	$('#load_pubmed')
+		.click(function(){
+			query_url = $("#pubmed_query_url").text()
+			pmid = $('input[name="pmid"]').val()
+			pmiderror = $("#pmiderror")
+			
+			pmiderror.text("Querying...")
+			pmiderror.show()
+			
+			if(/^[0-9]+$/.test(pmid)){
+				$.get(query_url + pmid, function(data) {
+					  date = data['DA'];
+					  year = date.substring(0,4);
+					  month = date.substring(4,6)<<0;
+					  month_val = month_vals[month - 1];
+					  
+					  volume = data['VI'];
+					  pages = data['PG'].split("-");
+					  journal = data['JT'];
+					  authors = data['AU'].join();
+					  
+					  $('input[name="authors"]').val(authors);
+					  $('input[name="journal"]').val(journal);
+					  $('select[name="publication_month"]').val(month_val);
+					  $('input[name="publication_year"]').val(year);
+					  $('input[name="volume"]').val(volume);
+					  $('input[name="page_start"]').val(pages[0]);
+					  $('input[name="page_end"]').val(pages[1]);
+					  
+					  pmiderror.hide();
+					}, 'json')
+					.error(function() { 
+							pmiderror.text("Pubmed query failed!")
+						});
+			}else {
+				pmiderror.text("PMID must be an integer!")
+			}
+			
+			
+		});
 });
