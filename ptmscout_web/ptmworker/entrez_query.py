13432f9ca5cb6fbc4fb0ca467021e870c6159c2c
Matthew Matlock <matt.matlock@gmail.com>
2012-11-05 18:09:02 -0600

    No longer need to be able to query entrez for protein records, using Geeneus for queries

--- ptmscout_web/ptmworker/entrez_query.py
+++ ptmscout_web/ptmworker/entrez_query.py
@@ -2,32 +2,6 @@ from celery.task import task
 from Bio import Entrez, SeqIO
 from ptmscout.config import settings
 from Bio import Medline
-
-@task(rate_limit='3/s', max_retries=5, default_retry_delay=1)
-def get_protein_records_by_accession(accessions):
-    Entrez.email = settings.adminEmail
-    search_handle = Entrez.esearch(db="protein", term=",".join(accessions), usehistory="y")
-    search_results = Entrez.read(search_handle)
-    search_handle.close()
-    
-    gi_list = search_results["IdList"]
-    webenv = search_results["WebEnv"]
-    query_key = search_results["QueryKey"] 
-    
-    fetch_handle = Entrez.efetch(db="protein", rettype='gb', retmode='text', id=gi_list, webenv=webenv, query_key=query_key)
-    record_iterator = SeqIO.parse(fetch_handle, "genbank")
-    
-    record_map = {}
-    try:
-        for r in record_iterator:
-            for acc in r.annotations['accessions']:
-                records = record_map.get(acc, set())
-                records.add(r)
-                record_map[acc] = records
-    except IndexError:
-        pass
-    
-    return record_map
     
 @task(rate_limit='3/s', max_retries=5, default_retry_delay=1)
 def get_pubmed_record_by_id(pmid):
--- ptmscout_web/tests/ptmworker/test_ptmworker_entrez_query.py
+++ ptmscout_web/tests/ptmworker/test_ptmworker_entrez_query.py
@@ -1,6 +1,6 @@
 # Skip
 from tests.PTMScoutTestCase import IntegrationTestCase
-from ptmworker.entrez_query import get_pubmed_record_by_id, get_protein_records_by_accession
+from ptmworker.entrez_query import get_pubmed_record_by_id
 from ptmscout.config import settings
 from Bio import Entrez
 
@@ -14,11 +14,3 @@ class EntrezQueryTestCase(IntegrationTestCase):
         self.assertEqual('The Bio* toolkits--a brief overview.', record['TI'])
         self.assertEqual('296-302', record['PG'])
         self.assertEqual('2002 Sep', record['DP'])
-
-    def test_get_protein_records(self):
-        accessions = ['XP_619639', 'P07197', 'Q14160', 'O15320', 'Q8N9T8', 'P51858', 'Q9H6F5']
-        records = get_protein_records_by_accession(accessions)
-        
-        self.assertEqual(7, len(records))
-        for acc in accessions:
-            self.assertEqual(1, len(records[acc]))
