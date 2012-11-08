30619015cf2d4a6f82f59400728f0ddbeed0f18a
Matthew Matlock <matt.matlock@gmail.com>
2012-11-07 18:34:12 -0600

    Restart checks to see if service is running before attempting kill of pceleryd

--- ptmscout_web/restart.sh
+++ ptmscout_web/restart.sh
@@ -4,8 +4,11 @@ sudo chmod -R 775 *
 sudo chown -R www-data *
 sudo chgrp -R development *
 
-kill `cat "logs/pids"`
-/data/pyramid/bin/pceleryd development.ini --logfile=logs/pceleryd --loglevel=DEBUG --pidfile=logs/pids > /dev/null &
+if [ -e "logs/pids" ]
+then
+    kill `cat "logs/pids"`
+fi
 
+/data/pyramid/bin/pceleryd development.ini --logfile=logs/pceleryd --loglevel=DEBUG --pidfile=logs/pids > /dev/null &
 sudo apache2ctl restart
 
