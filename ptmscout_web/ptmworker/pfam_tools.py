5997f5a2a97c6f741b0af4c53e0304dfa1ff9045
Matthew Matlock <matt.matlock@gmail.com>
2012-11-13 16:28:44 -0600

    Added pfam query tools

---
+++ ptmscout_web/ptmworker/pfam_tools.py
@@ -0,0 +1,122 @@
+import urllib, urllib2
+import xml.dom.minidom as xml 
+import time
+
+class PFamDomain(object):
+    def __init__(self):
+        self.accession = None
+        self.label = None
+        self.start = None
+        self.stop = None
+        self.p_value = None
+        self.release = None
+        self.class_ = None
+        self.significant = None
+        
+
+class PFamParser(object):
+    
+    def __init__(self, pfam_xml):
+        dom = xml.parseString(pfam_xml).getElementsByTagName('results')[0]
+        self.domains = []
+        self.parseDom(dom)
+        
+    def __get_node_text(self, node):
+        txt = ""
+        for c in node.childNodes:
+            txt += str(c.nodeValue) 
+        return txt
+
+    def parseDom(self, dom):
+        for node in dom.getElementsByTagName('matches'):
+            for pnode in node.getElementsByTagName('protein'):
+                self.parseProtein(pnode)
+    
+    def parseProtein(self, dom):
+        for dnode in dom.getElementsByTagName('database'):
+            self.parseDB(dnode)
+    
+    def parseDB(self, dom):
+        db_release = dom.getAttribute('release')
+        
+        for node in dom.getElementsByTagName('match'):
+            self.parseMatch(node, db_release)
+    
+    def parseMatch(self, dom, db_release):
+        match_id = dom.getAttribute('id')
+        match_class = dom.getAttribute('class')
+        match_acc =  dom.getAttribute('accession')
+        
+        for node in dom.getElementsByTagName('location'):
+            self.parseLocation(node, match_acc, match_class, match_id, db_release)
+        
+    def parseLocation(self, dom, accession, class_, label, db_release):
+        pep_start = int(dom.getAttribute('start'))
+        pep_end = int(dom.getAttribute('end'))
+        pvalue = float(dom.getAttribute('evalue'))
+        issignificant = int(dom.getAttribute('significant'))
+        
+        domain = PFamDomain()
+        domain.accession = accession
+        domain.label = label
+        domain.start = pep_start
+        domain.end = pep_end
+        domain.p_value = pvalue
+        domain.significant = issignificant
+        domain.release = db_release
+        domain.class_ = class_
+        
+        self.domains.append(domain)
+        
+TIMEOUT=5
+class PFamError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __repr__(self):
+        return self.msg
+
+
+def filter_domains(domains):
+    domains = [domain for domain in domains if domain.class_=="Domain" and domain.significant==1]
+    domains = sorted(domains, key=lambda domain: domain.p_value)
+    
+    used_sites = set()
+    chosen_domains = []
+    
+    while( len(domains) > 0):
+        domain = domains.pop(0)
+        range_set = set(range(domain.start, domain.end+1))
+        
+        if len(range_set & used_sites) == 0:
+            used_sites |= range_set
+            chosen_domains.append(domain)
+    
+    return chosen_domains
+
+
+def get_computed_pfam_domains(prot_seq, cutoff):
+    args = {'seq':prot_seq, 'output':'xml', 'evalue':cutoff}
+    
+    jobrequest = urllib2.urlopen("http://pfam.sanger.ac.uk/search/sequence", urllib.urlencode(args))
+    
+    dom = xml.parseString(jobrequest.read())
+    result_url = dom.getElementsByTagName('result_url')[0].childNodes[0].nodeValue
+    print result_url
+    
+    code = 202
+    started = time.clock()
+    while(code == 202):
+        if time.clock() - started > TIMEOUT:
+            raise PFamError("Request Timed Out") 
+        
+        resultquery = urllib2.urlopen(result_url)
+        code = resultquery.getcode()
+        time.sleep(0.1)
+    
+    if code != 200:
+        raise PFamError("Got unexpected response code: %d" % (code))
+    
+    parsed_pfam = PFamParser(resultquery.read())
+    
+    return filter_domains(parsed_pfam.domains)
+
---
+++ ptmscout_web/tests/ptmworker/test_pfam_tools.py
@@ -0,0 +1,156 @@
+from tests.PTMScoutTestCase import IntegrationTestCase
+from ptmworker import pfam_tools
+
+class IntegrationTestPFamQuery(IntegrationTestCase):
+    
+    def test_pfam_parser(self):
+        pfam_xml = \
+"""<?xml version="1.0" encoding="UTF-8"?>
+<pfam xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xmlns="http://pfam.sanger.ac.uk/"
+      xsi:schemaLocation="http://pfam.sanger.ac.uk/
+                          http://pfam.sanger.ac.uk/static/documents/schemas/results.xsd"
+      release="26.0" 
+      release_date="2011-11-17">
+  <results job_id="8b00a2c5-ccef-4e55-b7cf-c1f9c500b21a">
+    <matches>
+      <protein length="280">
+        <database id="pfam" release="26.0" release_date="2011-11-17">
+          <match accession="PF13465.1" id="zf-H2C2_2" type="Pfam-A" class="Domain">
+            <location start="185" end="210" ali_start="188" ali_end="209" hmm_start="4" hmm_end="25" evalue="0.00093" bitscore="19.2" evidence="hmmer v3.0" significant="1">
+              <hmm>
+                <![CDATA[rHlrkHtgekpyeCplCgktFk]]>
+              </hmm>
+              <match_string>
+                <![CDATA[ H+r+H+g+k  eC  C+kt+ ]]>
+              </match_string>
+              <pp>
+                <![CDATA[7******************975]]>
+              </pp>
+              <seq>
+                <![CDATA[QHQRIHSGKKLCECNECSKTCN]]>
+              </seq>
+              <raw>
+                <![CDATA[
+#HMM       rHlrkHtgekpyeCplCgktFk
+#MATCH      H+r+H+g+k  eC  C+kt+ 
+#PP        7******************975
+#SEQ       QHQRIHSGKKLCECNECSKTCN
+                ]]>
+              </raw>
+            </location>
+            <location start="213" end="237" ali_start="215" ali_end="234" hmm_start="3" hmm_end="22" evalue="0.00094" bitscore="19.2" evidence="hmmer v3.0" significant="1">
+              <hmm>
+                <![CDATA[rrHlrkHtgekpyeCplCgk]]>
+              </hmm>
+              <match_string>
+                <![CDATA[ rH r+H+gekpyeC++ gk]]>
+              </match_string>
+              <pp>
+                <![CDATA[59*************97555]]>
+              </pp>
+              <seq>
+                <![CDATA[IRHHRIHSGEKPYECDKHGK]]>
+              </seq>
+              <raw>
+                <![CDATA[
+#HMM       rrHlrkHtgekpyeCplCgk
+#MATCH      rH r+H+gekpyeC++ gk
+#PP        59*************97555
+#SEQ       IRHHRIHSGEKPYECDKHGK
+                ]]>
+              </raw>
+            </location>
+            <location start="241" end="266" ali_start="241" ali_end="264" hmm_start="1" hmm_end="24" evalue="0.017" bitscore="15.2" evidence="hmmer v3.0" significant="1">
+              <hmm>
+                <![CDATA[nLrrHlrkHtgekpyeCplCgktF]]>
+              </hmm>
+              <match_string>
+                <![CDATA[+L+rHl++H g k +eC+  g+ F]]>
+              </match_string>
+              <pp>
+                <![CDATA[599**************9766555]]>
+              </pp>
+              <seq>
+                <![CDATA[DLVRHLKIHIGDKVLECSERGRIF]]>
+              </seq>
+              <raw>
+                <![CDATA[
+#HMM       nLrrHlrkHtgekpyeCplCgktF
+#MATCH     +L+rHl++H g k +eC+  g+ F
+#PP        599**************9766555
+#SEQ       DLVRHLKIHIGDKVLECSERGRIF
+                ]]>
+              </raw>
+            </location>
+          </match>
+          <match accession="PF13900.1" id="GVQW" type="Pfam-A" class="Domain">
+            <location start="61" end="112" ali_start="66" ali_end="112" hmm_start="7" hmm_end="51" evalue="0.024" bitscore="14.7" evidence="hmmer v3.0" significant="0">
+              <hmm>
+                <![CDATA[AGVqWhdLgSLQ..pppPrfkr..fsclslpsswdyrhhpprlanFvff]]>
+              </hmm>
+              <match_string>
+                <![CDATA[A+ ++    S +  +++P+++   +++++l + +++ hh+   ++Fvf+]]>
+              </match_string>
+              <pp>
+                <![CDATA[6666666667666667788877558999***********99..****97]]>
+              </pp>
+              <seq>
+                <![CDATA[ARLECSGPISAHcnLHLPGSSNspAPASQLAGITGTHHHTR--LIFVFL]]>
+              </seq>
+              <raw>
+                <![CDATA[
+#HMM       AGVqWhdLgSLQ..pppPrfkr..fsclslpsswdyrhhpprlanFvff
+#MATCH     A+ ++    S +  +++P+++   +++++l + +++ hh+   ++Fvf+
+#PP        6666666667666667788877558999***********99..****97
+#SEQ       ARLECSGPISAHcnLHLPGSSNspAPASQLAGITGTHHHTR--LIFVFL
+                ]]>
+              </raw>
+            </location>
+          </match>
+        </database>
+      </protein>
+    </matches>
+  </results>
+</pfam>
+"""
+        pfam_parser = pfam_tools.PFamParser(pfam_xml)
+        self.assertEqual(4, len(pfam_parser.domains))
+        
+        testdom = pfam_parser.domains[0]
+        self.assertEqual(185, testdom.start)
+        self.assertEqual(210, testdom.end)
+        self.assertEqual(1, testdom.significant)
+        self.assertEqual("zf-H2C2_2", testdom.label)
+        self.assertEqual("PF13465.1", testdom.accession)
+        self.assertEqual(0.00093, testdom.p_value)
+        self.assertEqual("26.0", testdom.release)
+        self.assertEqual("Domain", testdom.class_)
+        
+        testdom = pfam_parser.domains[3]
+        self.assertEqual(61, testdom.start)
+        self.assertEqual(112, testdom.end)
+        self.assertEqual(0, testdom.significant)
+        self.assertEqual("GVQW", testdom.label)
+        self.assertEqual("PF13900.1", testdom.accession)
+        self.assertEqual(0.024, testdom.p_value)
+        self.assertEqual("26.0", testdom.release)
+        self.assertEqual("Domain", testdom.class_)
+        
+    
+    def test_get_computed_pfam_domains(self):
+        prot_seq = "QTTEIYFLSFGGCKAVTRAQHGQVLVRAPWLPSHRVLTWQRERDPKPSGVFFVCLLFFGTESHTAARLECSGPISAHCNLHLPGSSNSPAPASQLAGITGTHHHTRLIFVFLVERGFHHAGQAGLELLSPADPPASVSQSAGITGVSHCAWPELSPYRTSENPYKVRERPYKCIGCGKDLKGSSQLIQHQRIHSGKKLCECNECSKTCNQSSHFIRHHRIHSGEKPYECDKHGKAFRWSSDLVRHLKIHIGDKVLECSERGRIFNQSSGLMQHQRNHTES"
+        cutoff = 0.1
+        domains = pfam_tools.get_computed_pfam_domains(prot_seq, cutoff)
+        self.assertEqual(3, len(domains))
+        
+        testdom = domains[0]
+        self.assertEqual(185, testdom.start)
+        self.assertEqual(210, testdom.end)
+        self.assertEqual(1, testdom.significant)
+        self.assertEqual("zf-H2C2_2", testdom.label)
+        self.assertEqual("PF13465.1", testdom.accession)
+        self.assertEqual(0.00093, testdom.p_value)
+        self.assertEqual("26.0", testdom.release)
+        self.assertEqual("Domain", testdom.class_)
+        
