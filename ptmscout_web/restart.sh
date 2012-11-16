5a29d10bcde1acc8c3a9d6214e40aa764e623010
Matthew Matlock <matt.matlock@gmail.com>
2012-11-16 11:46:36 -0600

    Redirecting logging

--- ptmscout_web/restart.sh
+++ ptmscout_web/restart.sh
@@ -1,14 +1,14 @@
-/data/pyramid/bin/python setup.py build
-/data/pyramid/bin/python setup.py develop
+/data/pyramid/bin/python setup.py build > logs/build.log
+/data/pyramid/bin/python setup.py develop > logs/deploy.log
 sudo chmod -R 775 *
 sudo chown -R www-data *
 sudo chgrp -R development *
 
-if [ -e "logs/pids" ]
+if [ -e "logs/ptmworker.pid" ]
 then
-    kill `cat "logs/pids"`
+    kill `cat "logs/ptmworker.pid"`
 fi
 
-/data/pyramid/bin/pceleryd development.ini --logfile=logs/pceleryd --loglevel=DEBUG --pidfile=logs/pids > /dev/null &
+/data/pyramid/bin/pceleryd development.ini --logfile=logs/ptmworker.log --loglevel=DEBUG --pidfile=logs/ptmworker.pid > logs/ptmworker.start &
 sudo apache2ctl restart
 
