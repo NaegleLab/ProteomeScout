8ebb7589585527624923952c6122c196e49df21c
Matthew Matlock <matt.matlock@gmail.com>
2012-11-26 16:57:22 -0600

    Added script to import gene ontology, modified relationship script, added installation instructions to setup initial GO database

--- ptmscout_web/scripts/import_gene_ontology_terms.py
+++ ptmscout_web/scripts/import_gene_ontology_terms.py
@@ -9,50 +9,49 @@ from DB_init import DatabaseInitialization
 from obo_parser import GeneOntologyFile
 
 if __name__ == '__main__':
+    
     settings = appconfig(os.path.join('config:', 'data', 'ptmscout', 'ptmscout_web', 'development.ini'))
     DatabaseInitialization.setUpClass(settings)
     dbinit = DatabaseInitialization()
 
     try:
         FLUSH_FREQUENCY = 1000
-            
+        
         dbinit.setUp()
         
         GO_file_location = "scripts/data/gene_ontology.1_2.obo"
         GO_file = GeneOntologyFile(GO_file_location)
-        
-        missing_terms = set()
 
-        i=0
+        print "Loading OBO:"
+        print "version: ", GO_file.format_version
+        print "date:    ", str(GO_file.date)
+
         term = GO_file.next_term()
-        print "inserting relationships."
-        while term != None:
-            dbterm = protein.getGoAnnotationById(term.id)
 
-            if dbterm == None:
-                missing_terms.add(term.id)
-            else:
-                for pid, pterm in term.is_a_relationships.items():
-                    parent_entry = protein.getGoAnnotationById(pid)
-                    if parent_entry == None:
-                        missing_terms.add(pid)
-                    else:
-                        parent_entry.children.append(dbterm)
-                
-            i+=1
-            if(i % FLUSH_FREQUENCY == 0):
+        print "inserting terms."
+        i = 0
+        new_go_terms_created = 0
+        while term != None:
+            if protein.getGoAnnotationById(term.id) == None:
+                dbterm = protein.GeneOntology()
+                dbterm.GO = term.id
+                dbterm.aspect = term.get_aspect()
+                dbterm.term = term.name
+                dbterm.date = GO_file.date
+                dbterm.version = GO_file.format_version
+                dbterm.save()
+                new_go_terms_created += 1
+
+            i += 1
+            if i % FLUSH_FREQUENCY == 0:
                 print ".",
                 sys.stdout.flush()
                 DBSession.flush()
 
             term = GO_file.next_term()
         
-        print "Missing terms:", len(missing_terms)
-        ofile = open('missing_terms', 'w')
-        for term in missing_terms:
-            ofile.write(term+"\n")
-        ofile.close()
-        
+        print "Created %d GO terms" % (new_go_terms_created)
+
         DBSession.flush()
     except Exception, e:
         traceback.print_exc()
