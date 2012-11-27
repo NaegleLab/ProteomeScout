5c2b6d5001c3c1375c35516c1f5d990e607c6b2a
Matthew Matlock <matt.matlock@gmail.com>
2012-11-27 14:23:58 -0600

    Added tooltips to go map

--- ptmscout_web/ptmscout/static/js/experiment_go.js
+++ ptmscout_web/ptmscout/static/js/experiment_go.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $(document).tooltip({ track: true });
 	json_base64 = d3.select(".GO_map").select(".data").text()
 	data = JSON.parse(Base64.decode(json_base64));
 	container = d3.select(".GO_map").append("div");
