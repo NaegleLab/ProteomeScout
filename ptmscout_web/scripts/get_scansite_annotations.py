c443b098cef462898d89c9484498129771951130
Matthew Matlock <matt.matlock@gmail.com>
2012-12-15 20:36:05 -0600

    refactored decorator to utils

--- ptmscout_web/scripts/get_scansite_annotations.py
+++ ptmscout_web/scripts/get_scansite_annotations.py
@@ -4,30 +4,7 @@ from paste.deploy.loadwsgi import appconfig
 import time
 from ptmworker import upload_helpers
 import sys
-
-def rate_limit(rate=None):
-    def wrap(fn):
-        def rate_limited_task(*args):
-            now = time.clock()
-            diff = now - rate_limited_task.last_call_time
-
-            if diff < rate_limited_task.seconds_per_task:
-                time.sleep(rate_limited_task.seconds_per_task - diff)
-
-            rval = fn(*args)
-
-            rate_limited_task.last_call_time = now
-            return rval
-
-        if rate != None:
-            rate_limited_task.seconds_per_task = 1.0 / float(rate)
-        else:
-            rate_limited_task.seconds_per_task = 0
-
-        rate_limited_task.last_call_time = 0
-        return rate_limited_task
-
-    return wrap
+from ptmscout.utils.decorators import rate_limit
 
 MOTIF_MAP = {
     'bos taurus':"MAMMALIAN",
