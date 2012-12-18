9e3565c830ff9a70e0896070f7b5a9c141cad2fd
Matthew Matlock <matt.matlock@gmail.com>
2012-12-17 23:52:15 -0600

    Fixed script to retrieve scansite annotations, only retrieves for peptides which have not been queried

--- ptmscout_web/scripts/get_scansite_annotations.py
+++ ptmscout_web/scripts/get_scansite_annotations.py
@@ -1,43 +1,63 @@
 from DB_init import DatabaseInitialization
 from ptmscout.database import DBSession, modifications, taxonomies
 from paste.deploy.loadwsgi import appconfig
-import time
+import time, datetime
 from ptmworker import upload_helpers
-import sys
+import sys, os
 from ptmscout.utils.decorators import rate_limit
+import traceback
 
 MOTIF_MAP = {
     'bos taurus':"MAMMALIAN",
+    'bubalus bubalis':"MAMMALIAN",
+    'callithrix jacchus':"MAMMALIAN",
+    'canis familiaris':"MAMMALIAN",
+    'cavia porcellus':"MAMMALIAN",
     'capra hircus':"MAMMALIAN",
+    'chlorocebus aethiops':"MAMMALIAN",
+    'cricetulus longicaudatus':"MAMMALIAN",
     'cricetulus griseus':"MAMMALIAN",
+    'cricetus cricetus':"MAMMALIAN",
+    'equus caballus':"MAMMALIAN",
     'homo sapiens':"MAMMALIAN",
+    'mesocricetus auratus':"MAMMALIAN",
     'mus musculus':"MAMMALIAN",
+    'mus musculus molossinus':"MAMMALIAN",
+    'mustela putorius furo':"MAMMALIAN",
     'oryctolagus cuniculus':"MAMMALIAN",
     'ovis aries':"MAMMALIAN",
     'rattus norvegicus':"MAMMALIAN",
     'sus scrofa':"MAMMALIAN",
-    'Felis catus':"MAMMALIAN"
+    'felis catus':"MAMMALIAN",
+#    'Asterina pectinifera'
+#    'Coturnix coturnix japonica'
 #    'drosophila melanogaster'
 #    'gallus gallus'
+#    'Meleagris gallopavo'
+#    'torpedo californica'
 #    'xenopus laevis'
 }
+
+FLUSH_EVERY=100
 
 @rate_limit(rate=10)
 def insert_scansite_predictions(pep):
     species_name = pep.protein.species.name
-    motif_class = MOTIF_MAP[species_name]
+    motif_class = MOTIF_MAP[species_name.lower()]
 
     predictions = upload_helpers.query_peptide_predictions(pep.pep_aligned, motif_class)
 
     if len(predictions) > 0:
-        pep.predictions.extend(predictions)
+        for p in predictions:
+            pep.predictions.append(p)
+        pep.scansite_date = datetime.datetime.now()
         DBSession.add(pep)
 
 
 
 def needs_scansite(pep):
     species_name = pep.protein.species.name
-    return species_name in MOTIF_MAP and len(pep.predictions) == 0
+    return species_name.lower() in MOTIF_MAP and pep.scansite_date==None
 
 
 if __name__ == "__main__":
@@ -49,14 +69,17 @@ if __name__ == "__main__":
         dbinit.setUp()
 
         i = 0
-        for pep in DBSession.query(modifications.Peptide):
+        print "Getting peptide data..."
+
+        peptides = DBSession.query(modifications.Peptide).all()
+        for pep in peptides:
             if needs_scansite(pep):
                 insert_scansite_predictions(pep)
 
-            i+=1
             if i % FLUSH_EVERY == 0:
-                print i
+                print i, "/", len(peptides)
                 DBSession.flush()
+            i+=1
         
         DBSession.flush()
     except Exception, e:
