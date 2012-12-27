ed490afb669216e67a4b9f8989bd02764754a18e
Matthew Matlock <matt.matlock@gmail.com>
2012-12-26 19:05:29 -0600

    Fixed log level in restart script

--- ptmscout_web/restart.sh
+++ ptmscout_web/restart.sh
@@ -6,8 +6,8 @@ then
     mkdir logs/
 fi
 
-/data/pyramid/bin/python setup.py build > logs/build.log
-/data/pyramid/bin/python setup.py $arg > logs/deploy.log
+sudo /data/pyramid/bin/python setup.py build > logs/build.log
+sudo /data/pyramid/bin/python setup.py $arg > logs/deploy.log
 sudo chmod -R 775 *
 sudo chown -R www-data *
 sudo chgrp -R development *
@@ -25,7 +25,7 @@ if [ "$arg" == "develop" ]
 then
     /data/pyramid/bin/pceleryd development.ini --logfile=logs/ptmworker.log --loglevel=DEBUG --pidfile=logs/ptmworker.pid > logs/ptmworker.start &
 else
-    /data/pyramid/bin/pceleryd production.ini --logfile=logs/ptmworker.log --loglevel=DEBUG --pidfile=logs/ptmworker.pid > logs/ptmworker.start &
+    /data/pyramid/bin/pceleryd production.ini --logfile=logs/ptmworker.log --loglevel=INFO --pidfile=logs/ptmworker.pid > logs/ptmworker.start &
 fi
 sudo apache2ctl restart
 
