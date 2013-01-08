7e0dd1c5f21a85516a35685d0d1c079569e9606f
Matthew Matlock <matt.matlock@gmail.com>
2013-01-08 15:03:59 -0600

    Added script to deploy app without rebooting worker threads

---
+++ ptmscout_web/deploy.sh
@@ -0,0 +1,17 @@
+defmode='install'
+arg=${1:-$defmode}
+
+if [ ! -e logs ]
+then
+    mkdir logs/
+fi
+
+echo "Deploying codebase..."
+sudo /data/pyramid/bin/python setup.py build > logs/build.log
+sudo /data/pyramid/bin/python setup.py $arg > logs/deploy.log
+sudo chmod -R 775 *
+sudo chown -R www-data *
+sudo chgrp -R development *
+
+echo "Restarting webservice..."
+sudo apache2ctl restart
