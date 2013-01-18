b5485338515e3939aa89eb701a47d4867b92e7a1
Matthew Matlock <matt.matlock@gmail.com>
2013-01-18 11:09:12 -0600

    Data import sets status directly, uses new loading stage 'in queue'

--- ptmscout_web/ptmworker/data_import.py
+++ ptmscout_web/ptmworker/data_import.py
@@ -17,7 +17,7 @@ def start_import(exp_id, session_id, user_email, application_url):
     log.info("Loading data file...")
     accessions, peptides, mod_map, data_runs, errors, line_mapping = upload_helpers.parse_datafile(session)
 
-    if exp.loading_stage == 'query' or exp.status != 'error':
+    if exp.loading_stage == 'in queue' or exp.status != 'error':
         #exp.clearErrors()
         log.info("Reporting data file errors...")
         upload_helpers.report_errors(exp_id, errors, line_mapping)
@@ -29,7 +29,9 @@ def start_import(exp_id, session_id, user_email, application_url):
     else:
         headers = upload_helpers.get_series_headers(session)
 
-        notify_tasks.set_loading_status.apply_async((exp_id, 'loading'))
+        exp.status = 'loading'
+        experiment.setExperimentProgress(exp_id, 0, 0)
+        exp.saveExperiment()
 
         query_task = protein_tasks.get_proteins_from_external_databases.s(accessions, exp_id, line_mapping)
         proteins_task = protein_tasks.query_protein_metadata.s(accessions, exp_id, line_mapping)
