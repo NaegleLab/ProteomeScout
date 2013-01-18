10a6929ba3f47e24947b54b210d41f9920d97391
Matthew Matlock <matt.matlock@gmail.com>
2013-01-18 11:08:41 -0600

    Added queues for all loading tasks (concurrency=1)

--- ptmscout_web/restart.sh
+++ ptmscout_web/restart.sh
@@ -20,13 +20,17 @@ then
     loglevel="DEBUG"
     config="development.ini"
 else
-    loglevel="INFO"
+    loglevel="DEBUG"
     config="production.ini"
 fi
 
 echo "Starting workers..."
-/data/pyramid/bin/pceleryd $config -Q celery --concurrency=11 --logfile=logs/ptmworker.log --loglevel=$loglevel --pidfile=logs/ptmworker.0.pid > logs/ptmworker.0.start &
+/data/pyramid/bin/pceleryd $config -Q celery --concurrency=7 --logfile=logs/ptmworker.log --loglevel=$loglevel --pidfile=logs/ptmworker.0.pid > logs/ptmworker.0.start &
 /data/pyramid/bin/pceleryd $config -Q notify --concurrency=1 --logfile=logs/ptmworker.log --loglevel=$loglevel --pidfile=logs/ptmworker.1.pid > logs/ptmworker.1.start &
+/data/pyramid/bin/pceleryd $config -Q query_proteins --concurrency=1 --logfile=logs/ptmworker.log --loglevel=$loglevel --pidfile=logs/ptmworker.2.pid > logs/ptmworker.2.start &
+/data/pyramid/bin/pceleryd $config -Q load_proteins --concurrency=1 --logfile=logs/ptmworker.log --loglevel=$loglevel --pidfile=logs/ptmworker.3.pid > logs/ptmworker.3.start &
+/data/pyramid/bin/pceleryd $config -Q load_go_terms --concurrency=1 --logfile=logs/ptmworker.log --loglevel=$loglevel --pidfile=logs/ptmworker.4.pid > logs/ptmworker.4.start &
+/data/pyramid/bin/pceleryd $config -Q load_peptides --concurrency=1 --logfile=logs/ptmworker.log --loglevel=$loglevel --pidfile=logs/ptmworker.5.pid > logs/ptmworker.5.start &
 
 echo "Restarting webservice..."
 sudo apache2ctl restart
