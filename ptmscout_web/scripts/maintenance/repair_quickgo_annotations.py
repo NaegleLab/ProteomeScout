6dda048a773461f9fd04e830dfe52e67a1fbd328
Matthew Matlock <matt.matlock@gmail.com>
2013-01-29 17:21:24 -0600

    Added script to repair existing GO entries

---
+++ ptmscout_web/scripts/maintenance/repair_quickgo_annotations.py
@@ -0,0 +1,123 @@
+from scripts.DB_init import DatabaseInitialization
+from ptmscout.database import DBSession, protein
+from paste.deploy.loadwsgi import appconfig
+from ptmworker.helpers import quickgo_tools
+from ptmworker import GO_tasks
+import sys, os
+import traceback
+import logging
+import pickle
+
+FLUSH_EVERY = 100
+
+def update_hierarchy_links(term_entries):
+    changed_terms = {}
+    links = 0
+    for goId in term_entries:
+        go_term = protein.getGoAnnotationById(goId)
+        entry = term_entries[goId]
+
+        for parentId in entry.is_a:
+            parent_term = protein.getGoAnnotationById(parentId)
+
+            if not parent_term.hasChild(goId):
+                parent_term.children.append(go_term)
+                changed_terms[parentId] = parent_term
+                links += 1
+
+    print "Created %d new edges" % (links)
+
+    for goId in changed_terms:
+        changed_terms[goId].save()
+
+
+def get_term(goId, missing_terms):
+    version, entry = quickgo_tools.get_GO_term(goId)
+
+    for parentId in entry.is_a:
+        if protein.getGoAnnotationById(parentId) == None:
+            missing_terms.add(parentId)
+
+    return entry, version
+
+def get_all_parents(missing_terms, term_entries):
+    created = 0
+    while len(missing_terms) > 0:
+        goId = missing_terms.pop()
+        if goId in term_entries:
+            continue
+
+        entry, version = get_term(goId, missing_terms)
+        term_entries[goId] = entry
+
+        go_term = protein.GeneOntology()
+        go_term.GO = goId
+        go_term.term = entry.goName
+        go_term.aspect = entry.goFunction
+        go_term.version = version
+        go_term.save()
+
+        created += 1
+
+    print "Created %d new GO terms" % ( created )
+
+
+def get_all_terms():
+    term_entries = {}
+    missing_terms = set()
+
+    go_term_cnt = DBSession.query(protein.GeneOntology).count()
+    i = 0
+    for go_term in DBSession.query(protein.GeneOntology):
+        goId = go_term.GO
+        entry, _v = get_term(goId, missing_terms)
+        term_entries[goId] = entry
+
+        i+=1
+        if i % FLUSH_EVERY == 0:
+            print "%d / %d" % (i, go_term_cnt)
+
+    return term_entries, missing_terms
+
+if __name__ == "__main__":
+    try:
+        settings = appconfig(os.path.join('config:', 'data', 'ptmscout', 'ptmscout_web', 'production.ini'))
+
+        if len(sys.argv) > 1 and sys.argv[1] == '--force':
+            os.remove('repair_go.entries')
+
+        DatabaseInitialization.setUpClass(settings)
+        dbinit = DatabaseInitialization()
+        dbinit.setUp()
+        dbinit.log_capture('ptmscout', level=logging.INFO)
+
+        tmp_cache_filename = 'repair_go.entries'
+
+        if os.path.exists(tmp_cache_filename):
+            print "Loading terms from cache..."
+            with open(tmp_cache_filename, 'r') as cache_file:
+                term_entries, missing_terms = pickle.loads(cache_file.read())
+        else:
+            print "Loading terms from quickgo..."
+            term_entries, missing_terms = get_all_terms()
+
+            print "Saving terms to cache..."
+            with open(tmp_cache_filename, 'w') as cache_file:
+                cache_file.write(pickle.dumps( (term_entries, missing_terms) ))
+
+        print "Found %d missing parent terms" % ( len(missing_terms) )
+        print "Getting missing parent terms..."
+        get_all_parents(missing_terms, term_entries)
+
+        print "Updating is_a relationships..."
+        update_hierarchy_links(term_entries)
+
+        DBSession.flush()
+    except Exception, e:
+        traceback.print_exc()
+        
+        print "Rolling back database changes..."
+        dbinit.rollback()
+    else:
+        print "Finalizing DB changes"
+        dbinit.tearDown()
