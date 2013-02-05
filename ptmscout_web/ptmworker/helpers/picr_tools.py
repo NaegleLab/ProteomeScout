e3ffde23821c17baa76002cb49296a8e02b87cab
Matthew Matlock <matt.matlock@gmail.com>
2013-02-05 13:25:07 -0600

    Updated picr timeout to 15 seconds

--- ptmscout_web/ptmworker/helpers/picr_tools.py
+++ ptmscout_web/ptmworker/helpers/picr_tools.py
@@ -33,7 +33,7 @@ class PICRParser(object):
 
 picr_accession_query_url = "http://www.ebi.ac.uk/Tools/picr/rest/getUPIForAccession?accession=%s&taxid=%d&database=IPI&database=REFSEQ&database=SWISSPROT"
 picr_sequence_query_url  = "http://www.ebi.ac.uk/Tools/picr/rest/getUPIForSequence?sequence=%s&taxid=%d&database=IPI&database=REFSEQ&database=SWISSPROT"
-PICR_QUERY_TIMEOUT=10
+PICR_QUERY_TIMEOUT=15
 PICR_QUERY_RETRIES=3
 
 
