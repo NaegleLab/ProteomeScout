c62d24a923d304ee606889446b859cf56e46f0c1
Matthew Matlock <matt.matlock@gmail.com>
2013-02-08 13:17:54 -0600

    Implemented modal wait for any class:longtask clickable

--- ptmscout_web/ptmscout/static/js/ptmscout.js
+++ ptmscout_web/ptmscout/static/js/ptmscout.js
@@ -3,19 +3,21 @@ $(function(){
         .on('click', function(e) {
             $("#waiting-dialog")
                 .dialog({
-                    height: 100,
+                    height: 150,
+                    width: 150,
                     modal: true,
                     draggable: false,
                     resizable: false
                 });
         });
+
     $("div.progress").each(function(){
             var val = $(this).text();
             $(this).text("");
             var items = val.split(" / ");
             var n = parseInt(items[0]);
             var d = parseInt(items[1]);
-
+            
             if(n == 0){
                 $(this).progressbar({ value: false });
             }else{
