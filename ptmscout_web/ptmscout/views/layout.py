47a62d5cb76460304b73fd80013c46fc4a6783f4
John Naegle <john.naegle@gmail.com>
2013-03-28 22:23:34 -0500

    refactor the experimentSummaryMenu into a macro so it is defined in one place

--- ptmscout_web/ptmscout/views/layout.py
+++ ptmscout_web/ptmscout/views/layout.py
@@ -14,6 +14,7 @@ def add_path_definitions(event):
     event['experiment'] = None
 
     event['experiment_header'] = experiment_template()
+    event['experiment_summary_menu'] = experiment_summary_template()    
     event['experiment_list'] = experiment_list_template()
     event['protein_header'] = protein_template()
     event['protein_list'] = protein_list_template()
@@ -29,6 +30,11 @@ def site_layout():
 def experiment_template():
     renderer = get_renderer("ptmscout:templates/macro/experiment_header.pt")
     legend = renderer.implementation().macros['experiment_header']
+    return legend
+
+def experiment_summary_template():
+    renderer = get_renderer("ptmscout:templates/macro/experiment_summary_menu.pt")
+    legend = renderer.implementation().macros['experiment_summary_menu']
     return legend
 
 def protein_template():
