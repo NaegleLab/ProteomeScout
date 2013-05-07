d625d1a32136c02635c77889af2a4bd210127535
Matthew Matlock <matt.matlock@gmail.com>
2013-05-07 17:39:00 -0500

    Paging query results for memory reduction

--- ptmscout_web/scripts/maintenance/migrate_scansite_table.py
+++ ptmscout_web/scripts/maintenance/migrate_scansite_table.py
@@ -4,6 +4,25 @@ from ptmscout.database import DBSession, modifications, protein
 import os, sys
 import traceback
 
+YIELD_PER = 10000
+
+def page_query(query_generator, start=0):
+    offset = start
+    while True:
+        r = False
+        for elem in query_generator(YIELD_PER, offset):
+            r = True
+            yield elem
+            offset += YIELD_PER
+
+        if not r: break
+
+def page_peptides():
+    def query_generator(limit, offset):
+        sq = DBSession.query(modifications.Peptide.id).order_by(modifications.Peptide.id).limit(limit).offset(offset).subquery()
+        return DBSession.query(modifications.Peptide).join(sq, modifications.Peptide.id == sq.c.id)
+    return page_query( query_generator )
+
 if __name__ == "__main__":
     try:
         settings = os.path.join(os.sep, 'data', 'ptmscout', 'ptmscout_web', sys.argv[1])
@@ -14,9 +33,13 @@ if __name__ == "__main__":
 
         total_peptides = DBSession.query(modifications.Peptide).count()
         i = 0
+
         pb = progressbar.ProgressBar(max_value = total_peptides)
         pb.start()
-        for pep in DBSession.query(modifications.Peptide):
+
+
+        j = 0
+        for pep in page_peptides():
             for pred in pep.predictions:
                 ps = protein.ProteinScansite()
                 
@@ -28,13 +51,18 @@ if __name__ == "__main__":
                 
                 if not pep.protein.hasPrediction(ps.source, ps.value, ps.site_pos):
                     pep.protein.scansite.append(ps)
+                    j += 1
                 
                 pep.protein.saveProtein()
         
             i+=1
             pb.update(i)
+            if i % 1000 == 0:
+                DBSession.flush()
             
         pb.finish()
+
+        sys.stderr.write("Wrote %d scansite annotations\n" % (j))
         DBSession.flush()
     except Exception, e:
         traceback.print_exc()
