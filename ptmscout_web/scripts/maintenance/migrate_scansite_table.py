f35f2f2e555d66da2e8f6505887a9868506ed809
Matthew Matlock <matt.matlock@gmail.com>
2013-05-07 18:09:01 -0500

    Fixed bug...

--- ptmscout_web/scripts/maintenance/migrate_scansite_table.py
+++ ptmscout_web/scripts/maintenance/migrate_scansite_table.py
@@ -4,7 +4,7 @@ from ptmscout.database import DBSession, modifications, protein
 import os, sys
 import traceback
 
-YIELD_PER = 10000
+YIELD_PER = 1000
 
 def page_query(query_generator, start=0):
     offset = start
@@ -13,13 +13,13 @@ def page_query(query_generator, start=0):
         for elem in query_generator(YIELD_PER, offset):
             r = True
             yield elem
-            offset += YIELD_PER
 
+        offset += YIELD_PER
         if not r: break
 
 def page_peptides():
     def query_generator(limit, offset):
-        sq = DBSession.query(modifications.Peptide.id).order_by(modifications.Peptide.id).limit(limit).offset(offset).subquery()
+        sq = DBSession.query(modifications.Peptide.id).order_by(modifications.Peptide.id).offset(offset).limit(limit).subquery()
         return DBSession.query(modifications.Peptide).join(sq, modifications.Peptide.id == sq.c.id)
     return page_query( query_generator )
 
@@ -32,11 +32,12 @@ if __name__ == "__main__":
         dbinit.setUp()
 
         total_peptides = DBSession.query(modifications.Peptide).count()
+
+        sys.stderr.write( "Copying %d peptides\n" % total_peptides )
         i = 0
 
         pb = progressbar.ProgressBar(max_value = total_peptides)
         pb.start()
-
 
         j = 0
         for pep in page_peptides():
@@ -62,7 +63,7 @@ if __name__ == "__main__":
             
         pb.finish()
 
-        sys.stderr.write("Wrote %d scansite annotations\n" % (j))
+        sys.stderr.write("Processed %d peptides, wrote %d scansite annotations\n" % (i, j))
         DBSession.flush()
     except Exception, e:
         traceback.print_exc()
