5e97a5f512770856dcae6bdc748dd7af46e34d98
Matthew Matlock <matt.matlock@gmail.com>
2013-06-14 15:09:54 -0500

    Fixed decorator to take request = args[-1]

--- ptmscout_web/ptmscout/views/protein/decorators.py
+++ ptmscout_web/ptmscout/views/protein/decorators.py
@@ -1,13 +1,14 @@
 from ptmscout.utils.url_data import URLBuilder
 
 def experiment_filter(fn):
-    def build_filter(request):
+    def build_filter(*args):
         urlfilter = URLBuilder()
         urlfilter.create_field('experiment_id', URLBuilder.INTEGER)
+        request = args[-1]
         urlfilter.decode_request(request)
         request.urlfilter = urlfilter
 
-        return fn(request)
+        return fn(*args)
 
     build_filter.__name__ = fn.__name__
     return build_filter
