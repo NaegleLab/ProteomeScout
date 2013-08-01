babfa00ec4b289b67063a006ef5fcd0b0b308d39
Matthew Matlock <matt.matlock@gmail.com>
2013-08-01 14:59:15 -0500

    Ignoring glycosylation sites from uniprot download

--- ptmscout_web/ptmworker/helpers/uniprot_tools.py
+++ ptmscout_web/ptmworker/helpers/uniprot_tools.py
@@ -154,7 +154,7 @@ def parse_location(location):
         return location.start.real + 1, location.end.real
 
 def parse_features(features):
-    ignored_features = set(['chain', 'modified residue', 'splice variant', 'mutagenesis site', 'sequence conflict', 'sequence variant' ])
+    ignored_features = set(['chain', 'modified residue', 'splice variant', 'mutagenesis site', 'sequence conflict', 'sequence variant', 'glycosylation site' ])
     parsed = []
     for feature in features:
         if feature.type in ignored_features:
