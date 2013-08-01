0a07406508ef207de28d89ab48421dfb1157d54e
Matthew Matlock <matt.matlock@gmail.com>
2013-08-01 13:11:21 -0500

    Slight improvement to shared subset function, auto-selected correct annotation_set_id

--- ptmscout_web/ptmscout/views/dataset/share_subset_view.py
+++ ptmscout_web/ptmscout/views/dataset/share_subset_view.py
@@ -40,17 +40,21 @@ def view_shared_subset(context, request, exp):
     nsubset.name = "%s: %s" % (owner.username, nsubset.name)
     nsubset.save()
 
+    params = ""
     annotation_set_id = subset.annotation_set_id
-    if annotation_set_id is not None and not request.user.canViewAnnotations(annotation_set_id):
-        ap = annotations.AnnotationPermission()
-        ap.user_id = request.user.id
-        ap.annotation_set_id = annotation_set_id
-        ap.access_level = 'view'
+    if annotation_set_id is not None:
+        params = "?annotation_set=%d" % (annotation_set_id)
 
-        request.user.annotation_sets.append(ap)
-        request.user.saveUser()
+        if not request.user.canViewAnnotations(annotation_set_id):
+            ap = annotations.AnnotationPermission()
+            ap.user_id = request.user.id
+            ap.annotation_set_id = annotation_set_id
+            ap.access_level = 'view'
 
-    subset_page = request.route_url('experiment_subset', id=exp.id)
+            request.user.annotation_sets.append(ap)
+            request.user.saveUser()
+
+    subset_page = request.route_url('experiment_subset', id=exp.id) + params
     return {
             'pageTitle': strings.share_subsets_page_title % (subset.name),
             'header': strings.share_subsets_page_title % (subset.name),
