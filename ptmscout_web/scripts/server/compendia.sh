80a6b06eed5b8bd9452f65c6238490b21c32dfc5
Matthew Matlock <matt.matlock@gmail.com>
2014-03-11 10:15:19 -0500

    Renamed database dump files

--- ptmscout_web/scripts/server/compendia.sh
+++ ptmscout_web/scripts/server/compendia.sh
@@ -4,15 +4,15 @@ set -e
 source /data/pyramid/bin/activate
 
 cd /data/ptmscout/ptmscout_web
-python scripts/export/export_compendia.py > data/export/ptmscout_everything.tsv
-python scripts/export/export_compendia.py --species "vertebrata" > data/export/ptmscout_vertebrata.tsv
-python scripts/export/export_compendia.py --species "mammalia" > data/export/ptmscout_mammalia.tsv
-python scripts/export/export_compendia.py --modification "PHOS" > data/export/ptmscout_phosphorylation.tsv
-python scripts/export/export_compendia.py --modification "Acetylation" > data/export/ptmscout_acetylation.tsv
-python scripts/export/export_compendia.py --modification "METH" > data/export/ptmscout_methylation.tsv
-python scripts/export/export_compendia.py --modification "UBIQ" > data/export/ptmscout_ubiquitination.tsv
-python scripts/export/export_compendia.py --modification "Glycosylation" > data/export/ptmscout_glycosylation.tsv
+python scripts/export/export_compendia.py > data/export/proteomescout_everything.tsv
+python scripts/export/export_compendia.py --species "vertebrata" > data/export/proteomescout_vertebrata.tsv
+python scripts/export/export_compendia.py --species "mammalia" > data/export/proteomescout_mammalia.tsv
+python scripts/export/export_compendia.py --modification "PHOS" > data/export/proteomescout_phosphorylation.tsv
+python scripts/export/export_compendia.py --modification "Acetylation" > data/export/proteomescout_acetylation.tsv
+python scripts/export/export_compendia.py --modification "METH" > data/export/proteomescout_methylation.tsv
+python scripts/export/export_compendia.py --modification "UBIQ" > data/export/proteomescout_ubiquitination.tsv
+python scripts/export/export_compendia.py --modification "Glycosylation" > data/export/proteomescout_glycosylation.tsv
 
-python scripts/export/summarize_compendia.py ptmscout_everything.tsv ptmscout_vertebrata.tsv ptmscout_mammalia.tsv ptmscout_phosphorylation.tsv ptmscout_acetylation.tsv ptmscout_methylation.tsv ptmscout_ubiquitination.tsv ptmscout_glycosylation.tsv > data/export/listing.pyp
+python scripts/export/summarize_compendia.py proteomescout_everything.tsv proteomescout_vertebrata.tsv proteomescout_mammalia.tsv proteomescout_phosphorylation.tsv proteomescout_acetylation.tsv proteomescout_methylation.tsv proteomescout_ubiquitination.tsv proteomescout_glycosylation.tsv > data/export/listing.pyp
 
 python scripts/maintenance/generate_statistics.py
