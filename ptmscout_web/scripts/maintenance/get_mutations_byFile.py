87fe1ec543746c07d299eccfe6ee2ac1a38fce0a
Kristen Naegle <kmn4mj@virginia.edu>
2015-06-30 15:48:47 -0500

    moved mutations get to function in preparation for outer loop that can continue with a killed process

--- ptmscout_web/scripts/maintenance/get_mutations_byFile.py
+++ ptmscout_web/scripts/maintenance/get_mutations_byFile.py
@@ -11,59 +11,47 @@ DEBUG=False
 REPORT_EVERY = 250
 BATCH_SIZE = 500
 FLUSH_FREQ = 1000
-PICKLEFILE = './mutations/mutationsUpdate.pickle'
+LOGFILE = './mutations/logFile'
 FILEBASE = './mutations/accList_'
-if __name__ == "__main__":
+
+def get_mutations_for_accList(accFile, logFile):
+    flag = 0
+    fLog = open(logFile, 'a')
     try:
-        dbconfig = os.path.join(os.sep, 'data', 'ptmscout', 'ptmscout_web', 'production.ini')
-        
-        DatabaseInitialization.setUpClass(dbconfig)
-        dbinit = DatabaseInitialization()
-        dbinit.setUp()
 
-        manager = Proteome.ProteinManager(settings.adminEmail, cache=True, retry=2) # but remember to purge!
-
-	#prot_map = pickle.load(open(PICKLEFILE, "r"))
-	#	pcnt = len(prot_map)
-        
+	manager = Proteome.ProteinManager(settings.adminEmail, cache=True, retry=2) # but remember to purge!
         i = 0
-	fileNums = [1]
 	queryBad = []
-        for batch in fileNums:
-            i+=1
-	    # read in all of the accssions 
-	    fileName = FILEBASE+str(batch)
-	    with open (fileName, 'r') as f:
+	i+=1
+	with open (accFile, 'r') as f:
 		batch_query = [line.rstrip() for line in f]
-	    print "there are %d accessions in %s"%(len(batch_query), fileName)
-	    try:
-		    manager.batch_get_protein_name(batch_query)
-	    except Exception, e2: 
-		    print "Error in get_protein_name with error %s, attempting recovery"%(e2)
-		    for query in batch_query:
+		fLog.write("there are %d accessions in %s"%(len(batch_query), accFile))
+	try:
+		manager.batch_get_protein_name(batch_query)
+	except Exception, e2: 
+		fLog.write("Error in get_protein_name with error %s, attempting recovery"%(e2))
+		for query in batch_query:
 			try:
 				seq = manager.get_protein_sequence(query)
 				if seq is None:
-					print "Error getting sequence %s with error: %s, removing from protein map"%(query, e2)
+					fLog.write("Error getting sequence %s with error: %s, removing from protein map"%(query, e2))
 					queryBad.append(query)
-					protein_map.pop(query, None)
+					batch_query.remove(query)
 			except Exception, e3:
-				print "Error getting sequence %s with error: %s, removing from protein map"%(query, e3)
+				fLog.write("Error getting sequence %s with error: %s, removing from protein map"%(query, e3))
 				queryBad.append(query)
 				batch_query.remove(query)
-				protein_map.pop(query, None)
 			else:
-				print "Success for %s"%(query)
-	    else: 
-		print "No errors on that try catch for batch %s" %(fileName)
+				fLog.write("Success for %s"%(query))
+	else: 
+		fLog.write("No errors on that try catch for batch %s" %(accFile))
 	
-	print "Total bad queries:\n"
-	print queryBad
-        print "Building variant table..."
+	fLog.write("Total bad queries: for %s is %d \n"%(accFile, len(queryBad)))
+	for q in queryBad:
+		fLog.write("%s, "%(q))
         i = 0
         for acc in batch_query:
 	    prots = protein.getProteinsByAccession(acc)
-            #prot = prot_map[acc]
             prot = prots[0]
             prot_seq = manager.get_protein_sequence(acc)
             if prot_seq and prot.sequence == prot_seq.upper():
@@ -80,14 +68,14 @@ if __name__ == "__main__":
                                 mutantDict['mutant'], acc,
                                 mutantDict['notes'], prot.id)
                         if not new_mutation.consistent(prot_seq):
-                            print "Loaded mutation does not match protein sequence %s (%d %s) %s -> %s" % (acc, new_mutation.location, prot_seq[new_mutation.location-1], new_mutation.original, new_mutation.mutant)
+                            fLog.write("Loaded mutation does not match protein sequence %s (%d %s) %s -> %s" % (acc, new_mutation.location, prot_seq[new_mutation.location-1], new_mutation.original, new_mutation.mutant))
                         elif not prot.hasMutation(new_mutation):
                             j+=1
                             DBSession.add(new_mutation)
 
-                print "Added %d mutations for protein %d %s"  % (j, prot.id, acc)
+                fLog.write("Added %d mutations for protein %d %s"  % (j, prot.id, acc))
             else:
-                print "Warning: protein sequence mismatch for %d with %s" % (prot.id, acc)
+                fLog.write("Warning: protein sequence mismatch for %d with %s" % (prot.id, acc))
 
             i+=1
             if i % FLUSH_FREQ == 0:
@@ -97,9 +85,26 @@ if __name__ == "__main__":
     except Exception, e:
         traceback.print_exc()
         
-        print "Rolling back database changes..."
+        fLog.write("Rolling back database changes... for %s"%(accFile))
         dbinit.rollback()
+	flag = -1
     else:
         print "Finalizing DB changes"
-	print "Completed access list %s"%(fileName)
+	flag = 1
+	print "Completed access list %s"%(accFile)
         dbinit.tearDown()
+    fLog.close()
+    return flag, queryBad
+
+if __name__ == "__main__":
+        dbconfig = os.path.join(os.sep, 'data', 'ptmscout', 'ptmscout_web', 'production.ini')
+        
+        DatabaseInitialization.setUpClass(dbconfig)
+        dbinit = DatabaseInitialization()
+        dbinit.setUp()
+	
+	j = 0
+	accFile = FILEBASE+str(j)
+	flag, queryBad = get_mutations_for_accList(accFile, LOGFILE)
+	if(flag==1): 
+		print "Success for %s"%(accFile)
